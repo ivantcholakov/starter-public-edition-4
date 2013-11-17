8be087c5340708590f609591f6993c70bff52f3b
Ivan Tcholakov <ivantcholakov@gmail.com>
2013-11-17 04:40:51 +0200

    Minor corrections about admin login.

--- platform/applications/admin/core/Base_Controller.php
+++ platform/applications/admin/core/Base_Controller.php
@@ -50,6 +50,9 @@ class Base_Controller extends Core_Controller {
 
     protected function _login($username, $password) {
 
+        // Break the previous login if there is any.
+        $this->_logout();
+
         $this->session->set_userdata('user_logged', true);
 
         // This is a method for demo purpose, it always returns TRUE.
@@ -58,12 +61,17 @@ class Base_Controller extends Core_Controller {
 
     protected function _logout() {
 
+        // Don't do anything if a user has not been logged.
+        if (!$this->session->userdata('user_logged')) {
+            return;
+        }
+
         // If the public site and the administration site
         // share same session space, consider the following:
         //$this->session->unset_userdata('user_logged');
         //
 
-        $this->session->sess_destroy();
+        $this->session->sess_regenerate(true);
     }
 
     private function _check_access() {
