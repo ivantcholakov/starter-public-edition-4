47baef2e51efb156bb45d2ad7071380247e5ef7e
Ivan Tcholakov <ivantcholakov@gmail.com>
2013-12-23 02:37:43 +0200

    A minor change.

--- platform/core/common/modules/email/events.php
+++ platform/core/common/modules/email/events.php
@@ -161,11 +161,11 @@ class Common_Events_Email {
     public function email_test($to = null) {
 
         $to = $to == '' ? null : $to;
-        $subject = $this->settings->get('site_name').': Test Message';
+        $subject = '['.$this->settings->get('site_name').'] '.'Test Message';
         $body =
 '
     <h1>This is a message for testing purpose</h1>
-    <p>Greetings from the team of <a href="'.site_url().'">'.$this->settings->get('site_name').'</a>.</p>
+    <p>Greetings from the team of <a href="'.base_url().'">'.$this->settings->get('site_name').'</a>.</p>
 ';
 
         return Events::trigger('email', compact('to', 'subject', 'body'));
