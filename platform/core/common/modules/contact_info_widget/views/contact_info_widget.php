fbcc3b659562fe730836bf2709252a68272945b2
Ivan Tcholakov <ivantcholakov@gmail.com>
2013-12-24 08:25:52 +0200

    A minor change.

--- platform/core/common/modules/contact_info_widget/views/contact_info_widget.php
+++ platform/core/common/modules/contact_info_widget/views/contact_info_widget.php
@@ -1,5 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+if (!isset($contacts) || !is_array($contacts)) {
+    $contacts = array();
+}
+
 foreach ($contacts as $item) {
 
 ?>
