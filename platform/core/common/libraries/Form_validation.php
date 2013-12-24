e2c2b69ea54f123cfefdbdab4bfe95405023aee7
Ivan Tcholakov <ivantcholakov@gmail.com>
2013-12-24 19:55:27 +0200

    Reworking some validation rules for UTF-8 compatibility.

--- platform/core/common/libraries/Form_validation.php
+++ platform/core/common/libraries/Form_validation.php
@@ -4,11 +4,16 @@ class Form_validation extends CI_Form_validation {
 
     public $CI; // See https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc/wiki/Home
 
+    protected $is_utf8;
+
     public function __construct($rules = array()) {
 
         parent::__construct($rules);
 
+        $this->is_utf8 = strtolower(config_item('charset')) == 'utf-8';
+
         $this->CI->load->helper('checkbox');
+        $this->CI->load->helper('email');
     }
 
 
@@ -22,5 +27,74 @@ class Form_validation extends CI_Form_validation {
         (is_object($module)) AND $this->CI = &$module;
         return parent::run($group);
     }
+
+
+    // ---------------------------------------------------------------------
+    // Reworked validation rules.
+    // ---------------------------------------------------------------------
+
+    public function valid_email($str) {
+
+        return valid_email($str);
+    }
+
+    public function min_length($str, $val) {
+
+        if (!is_numeric($val)) {
+            return false;
+        } else {
+            $val = (int) $val;
+        }
+
+        return $this->is_utf8
+            ? ($val <= UTF8::strlen($str))
+            : ($val <= strlen($str));
+    }
+
+    public function max_length($str, $val) {
+
+        if (!is_numeric($val)) {
+            return false;
+        } else {
+            $val = (int) $val;
+        }
+
+        return $this->is_utf8
+            ? ($val >= UTF8::strlen($str))
+            : ($val >= strlen($str));
+    }
+
+    public function exact_length($str, $val) {
+
+        if (!is_numeric($val)) {
+            return false;
+        } else {
+            $val = (int) $val;
+        }
+
+        return $this->is_utf8
+            ? (UTF8::strlen($str) === $val)
+            : (strlen($str) === $val);
+    }
+
+    public function prep_url($str = '') {
+
+        // Added by Ivan Tcholakov, 08-DEC-2011.
+        $str = str_ireplace('http://', 'http://', $str);
+        $str = str_ireplace('https://', 'https://', $str);
+        //
+
+        if ($str == 'http://' OR $str == '') {
+            return '';
+        }
+
+        if (substr($str, 0, 7) != 'http://' && substr($str, 0, 8) != 'https://') {
+            $str = 'http://'.$str;
+        }
+
+        return $str;
+    }
+
+    // ---------------------------------------------------------------------
 
 }
