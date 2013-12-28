f9ea9f823af67c058bb0ffbe521f4a89e6ee48e3
Ivan Tcholakov <ivantcholakov@gmail.com>
2013-12-28 08:14:43 +0200

    Whitespace cleaning.

--- platform/core/common/third_party/MX/Controller.php
+++ platform/core/common/third_party/MX/Controller.php
@@ -39,13 +39,13 @@ require dirname(__FILE__).'/Base.php';
 class MX_Controller
 {
     public $autoload = array();
-    
+
     public function __construct()
     {
         $class = str_replace(CI::$APP->config->item('controller_suffix'), '', get_class($this));
         log_message('debug', $class." MX_Controller Initialized");
-        Modules::$registry[strtolower($class)] = $this;    
-        
+        Modules::$registry[strtolower($class)] = $this;
+
         /* copy a loader instance and initialize */
         $this->load = clone load_class('Loader');
         $this->load->initialize($this);
@@ -53,7 +53,7 @@ class MX_Controller
         /* autoload module items */
         $this->load->_autoloader($this->autoload);
     }
-    
+
     public function __get($class) {
         return CI::$APP->$class;
     }
