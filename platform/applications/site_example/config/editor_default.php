8e5784fae3861fee6547cc7d647aa6535433d930
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-01-12 18:53:00 +0200

    CKEditor configuration corrections.

--- platform/applications/site_example/config/editor_default.php
+++ platform/applications/site_example/config/editor_default.php
@@ -8,8 +8,8 @@ $config['config']['defaultLanguage'] = 'en';
 $config['config']['contentsLanguage'] = 'en';
 $config['config']['contentsLangDirection'] = 'ltr';
 $config['config']['contentsCss'] = array();
-$config['config']['contentsCss'][] = BASE_URL.'assets/css/lib/bootstrap-2/bootstrap.css';
-$config['config']['contentsCss'][] = BASE_URL.'assets/css/editor.css';
+$config['config']['contentsCss'][] = BASE_URL.'assets/css/lib/bootstrap-3/bootstrap.css';
+$config['config']['contentsCss'][] = BASE_URL.'assets/css/lib/editor.css';
 
 $config['config']['width'] = '';
 $config['config']['height'] = '100';
