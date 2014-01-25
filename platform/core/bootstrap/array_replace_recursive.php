5cb64a028ee330f43b8cd57ab06058e00dacefcc
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-01-25 06:35:59 +0200

    A correction for PHP 5.2 compatibility.

--- platform/core/bootstrap/array_replace_recursive.php
+++ platform/core/bootstrap/array_replace_recursive.php
@@ -10,26 +10,6 @@ if (!function_exists('array_replace_recursive')) {
      */
     function array_replace_recursive($array, $array1) {
 
-        function array_replace_recursive_recurse($array, $array1) {
-
-            foreach ($array1 as $key => $value) {
-
-                // Create new key in $array, if it is empty or not an array.
-                if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key]))) {
-                    $array[$key] = array();
-                }
-
-                // Overwrite the value in the base array.
-                if (is_array($value)) {
-                    $value = array_replace_recursive_recurse($array[$key], $value);
-                }
-
-                $array[$key] = $value;
-            }
-
-            return $array;
-        }
-
         // Handle the arguments, merge one by one.
         $args = func_get_args();
         $array = $args[0];
@@ -43,6 +23,26 @@ if (!function_exists('array_replace_recursive')) {
             if (is_array($args[$i])) {
                 $array = array_replace_recursive_recurse($array, $args[$i]);
             }
+        }
+
+        return $array;
+    }
+
+    function array_replace_recursive_recurse($array, $array1) {
+
+        foreach ($array1 as $key => $value) {
+
+            // Create new key in $array, if it is empty or not an array.
+            if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key]))) {
+                $array[$key] = array();
+            }
+
+            // Overwrite the value in the base array.
+            if (is_array($value)) {
+                $value = array_replace_recursive_recurse($array[$key], $value);
+            }
+
+            $array[$key] = $value;
         }
 
         return $array;
