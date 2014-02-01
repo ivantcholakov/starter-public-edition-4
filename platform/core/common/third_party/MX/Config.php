6f3e07bff203d72325f964e0483688a14ce26aae
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-02-01 12:01:12 +0200

    A correction.

--- platform/core/common/third_party/MX/Config.php
+++ platform/core/common/third_party/MX/Config.php
@@ -41,7 +41,7 @@ class MX_Config extends CI_Config
             return $this->item($file);
         }
 
-        $_module OR $_module = CI::$APP->router->fetch_module();
+        $_module OR (class_exists('CI') && $_module = CI::$APP->router->fetch_module());
         list($path, $file) = Modules::find($file, $_module, 'config/');
 
         if ($path === FALSE) {
