9ada6ae19f81614d55a86aeb04569de21880425c
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-02-24 17:33:14 +0200

    Isuue #6 - Renaming error 404 controller according to CI3 naming convention.

--- platform/applications/site_example/modules/error_404/controllers/Error_404_controller.php
+++ platform/applications/site_example/modules/error_404/controllers/Error_404_controller.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) { exit('No direct script access allowed.'); }
 
-class error_404_controller extends Base_Controller {
+class Error_404_controller extends Base_Controller {
 
     public function __construct() {
 
