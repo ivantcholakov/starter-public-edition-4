51760201befe80777509a42863214de7f0d1b341
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-02-25 23:24:44 +0200

    Making CKEditor language sensitive.

--- platform/core/common/config/editor_default.php
+++ platform/core/common/config/editor_default.php
@@ -3,10 +3,10 @@
 $config['basePath'] = DEFAULT_BASE_URL.'assets/js/ckeditor/';
 $config['config']['baseHref'] = http_build_url(DEFAULT_BASE_URL.'../');
 $config['config']['fullPage'] = false;
-$config['config']['language'] = 'en';
+$config['config']['language'] = get_instance()->lang->ckeditor();
 $config['config']['defaultLanguage'] = 'en';
-$config['config']['contentsLanguage'] = 'en';
-$config['config']['contentsLangDirection'] = 'ltr';
+$config['config']['contentsLanguage'] = get_instance()->lang->ckeditor();
+$config['config']['contentsLangDirection'] = get_instance()->lang->direction();
 $config['config']['contentsCss'] = array();
 $config['config']['contentsCss'][] = DEFAULT_BASE_URL.'assets/css/lib/bootstrap-3/bootstrap.css';
 $config['config']['contentsCss'][] = DEFAULT_BASE_URL.'assets/css/lib/editor.css';
