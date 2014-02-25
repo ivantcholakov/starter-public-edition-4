f583a292cd26179646e2e335aa2b0b1263e87fe5
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-02-26 00:38:31 +0200

    CKEditor settings correction.

--- platform/core/common/config/editor_admin.php
+++ platform/core/common/config/editor_admin.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
 
-$config['config']['filebrowserBrowseUrl'] = BASE_URL.'assets/kcfinder/browse.php?type=files&dir=files&cfg=admin';
-$config['config']['filebrowserImageBrowseUrl'] = BASE_URL.'assets/kcfinder/browse.php?type=images&dir=images&cfg=admin';
-$config['config']['filebrowserFlashBrowseUrl'] = BASE_URL.'assets/kcfinder/browse.php?type=flash&dir=flash&cfg=admin';
+$config['config']['filebrowserBrowseUrl'] = DEFAULT_BASE_URL.'assets/kcfinder/browse.php?type=files&dir=files&cfg=admin';
+$config['config']['filebrowserImageBrowseUrl'] = DEFAULT_BASE_URL.'assets/kcfinder/browse.php?type=images&dir=images&cfg=admin';
+$config['config']['filebrowserFlashBrowseUrl'] = DEFAULT_BASE_URL.'assets/kcfinder/browse.php?type=flash&dir=flash&cfg=admin';
 
-//$config['config']['filebrowserUploadUrl'] = BASE_URL.'assets/kcfinder/upload.php?type=files&dir=files&cfg=admin';
-//$config['config']['filebrowserImageUploadUrl'] = BASE_URL.'assets/kcfinder/upload.php?type=images&dir=images&cfg=admin';
-//$config['config']['filebrowserFlashUploadUrl'] = BASE_URL.'assets/kcfinder/upload.php?type=flash&dir=flash&cfg=admin';
+//$config['config']['filebrowserUploadUrl'] = DEFAULT_BASE_URL.'assets/kcfinder/upload.php?type=files&dir=files&cfg=admin';
+//$config['config']['filebrowserImageUploadUrl'] = DEFAULT_BASE_URL.'assets/kcfinder/upload.php?type=images&dir=images&cfg=admin';
+//$config['config']['filebrowserFlashUploadUrl'] = DEFAULT_BASE_URL.'assets/kcfinder/upload.php?type=flash&dir=flash&cfg=admin';
