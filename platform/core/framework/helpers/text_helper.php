6484dc9ce9cadb0266183abce785766b04334e99
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-03-05 08:39:39 +0200

    Upgrading CodeIgniter.

--- platform/core/framework/helpers/text_helper.php
+++ platform/core/framework/helpers/text_helper.php
@@ -157,8 +157,8 @@ if ( ! function_exists('ascii_to_entities'))
 				if (count($temp) === $count)
 				{
 					$number = ($count === 3)
-							? (($temp[0] % 16) * 4096) + (($temp[1] % 64) * 64) + ($temp[2] % 64)
-							: (($temp[0] % 32) * 64) + ($temp[1] % 64);
+						? (($temp[0] % 16) * 4096) + (($temp[1] % 64) * 64) + ($temp[2] % 64)
+						: (($temp[0] % 32) * 64) + ($temp[1] % 64);
 
 					$out .= '&#'.$number.';';
 					$count = 1;
@@ -222,7 +222,7 @@ if ( ! function_exists('entities_to_ascii'))
 		{
 			return str_replace(
 				array('&amp;', '&lt;', '&gt;', '&quot;', '&apos;', '&#45;'),
-						array('&', '<', '>', '"', "'", '-'),
+				array('&', '<', '>', '"', "'", '-'),
 				$str
 			);
 		}
