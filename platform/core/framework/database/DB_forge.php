38ba586c3d75d1cd761331b613052a088849db79
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-03-10 19:35:59 +0200

    Upgrading CodeIgniter.

--- platform/core/framework/database/DB_forge.php
+++ platform/core/framework/database/DB_forge.php
@@ -806,10 +806,14 @@ abstract class CI_DB_forge {
 					case 'ENUM':
 					case 'SET':
 						$attributes['CONSTRAINT'] = $this->db->escape($attributes['CONSTRAINT']);
+						$field['length'] = is_array($attributes['CONSTRAINT'])
+							? "('".implode("','", $attributes['CONSTRAINT'])."')"
+							: '('.$attributes['CONSTRAINT'].')';
+						break;
 					default:
 						$field['length'] = is_array($attributes['CONSTRAINT'])
-								? "('".implode("','", $attributes['CONSTRAINT'])."')"
-								: '('.$attributes['CONSTRAINT'].')';
+							? '('.implode(',', $attributes['CONSTRAINT']).')'
+							: '('.$attributes['CONSTRAINT'].')';
 						break;
 				}
 			}
