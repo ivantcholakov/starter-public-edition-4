c9f05d210a78b86d065844ff01a4ce785ffff445
Gwenael G <gwenael.gallon@mac.com>
2014-03-13 02:10:54 +0100

    Add French language

---
+++ platform/core/common/language/french/mailer_lang.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['mailer_disabled'] = 'Le service e-mail n\'a pas été activée.';
+$lang['mailer_disabled_error'] = 'Nous sommes désolés, le service e-mail n\'a pas été activée. Le message n\'a pas été envoyé.';
+$lang['mailer_error'] = 'Nous sommes désolés, votre message n\'a pas été envoyé en raison d\'un problème technique.';
+$lang['mailer_a_message_has_been_received'] = 'Un message a été reçu';
+$lang['mailer_a_message_has_been_received_from'] = 'Un message de';
+$lang['mailer_your_message_has_been_sent'] = 'Merci! Votre message a été envoyé.';
+$lang['mailer_confirmation_for_a_received_message'] = 'Confirmation d\'un message reçu';
+$lang['mailer_confirmation_for_a_received_message_from'] = 'Confirmation d\'un message reçu à partir de';
