0db4bda46dafe430e076f404c57e81a9338a6a74
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-03-23 07:58:10 +0200

    Insignificant changes.

--- platform/core/common/language/english/ui_lang.php
+++ platform/core/common/language/english/ui_lang.php
@@ -3,7 +3,6 @@
  * @author      Ivan Tcholakov <ivantcholakov@gmail.com>, 2014.
  * @license     The MIT License (MIT), http://opensource.org/licenses/MIT
  */
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['ui_hello'] = 'Hello, %s.';
