fe85212ca45d808963a2cb34b1937ecf162f0743
Gwenael G <gwenael.gallon@mac.com>
2014-03-25 00:16:57 +0100

    Fix French translation

--- platform/core/common/language/french/db_lang.php
+++ platform/core/common/language/french/db_lang.php
@@ -3,7 +3,7 @@
 $lang['db_invalid_connection_str'] = 'Impossible de d&eacute;terminer les param&egrave;tres de la base de donn&eacute;es avec l\'instruction de connection donn&eacute;e.';
 $lang['db_unable_to_connect'] = 'Impossible de se connecter &agrave; la base de donn&eacute;es en utilisant les param&egrave;tres fournis.';
 $lang['db_unable_to_select'] = 'Impossible de s&eacute;lectionner la base de donn&eacute;es demand&eacute;e : %s';
-$lang['db_unable_to_create'] = 'Impossible de cr&eacute;er la base de donn&eacute;es demand&eacute;e: %s';
+$lang['db_unable_to_create'] = 'Impossible de cr&eacute;er la base de donn&eacute;es demand&eacute;e : %s';
 $lang['db_invalid_query'] = 'La requ&ecirc;te que vous avez soumise n\'est pas valide.';
 $lang['db_must_set_table'] = 'Vous devez sp&eacute;cifier une table &agrave; utiliser pour effectuer votre requ&ecirc;te.';
 $lang['db_must_use_set'] = 'Vous devez utiliser la m&eacute;thode "set()" pour mettre &agrave; jour une entr&eacute;e.';
@@ -13,7 +13,7 @@ $lang['db_must_use_where'] = 'Il faut obligatoirement sp&eacute;cifier le crit&e
 $lang['db_del_must_use_where'] = 'Il faut obligatoirement sp&eacute;cifier le crit&egrave;re de s&eacute;lection "WHERE" pour supprimer une entr&eacute;e.';
 $lang['db_field_param_missing'] = 'R&eacute;cup&eacute;rer des champs requiert le nom de la table en param&egrave;tre.';
 $lang['db_unsupported_function'] = 'Cette fonctionnalit&eacute; n\'est pas disponible pour la base de donn&eacute;es que vous utilisez.';
-$lang['db_transaction_failure'] = 'Erreur de transaction: Rollback effectu&eacute;';
+$lang['db_transaction_failure'] = 'Erreur de transaction : Rollback effectu&eacute;';
 $lang['db_unable_to_drop'] = 'Impossible de supprimer la base de donn&eacute;es sp&eacute;cifi&eacute;e.';
 $lang['db_unsupported_feature'] = 'Fonctionnalit&eacute; non support&eacute;e sur le syst&egrave;me de gestion de base de donn&eacute;es que vous utilisez.';
 $lang['db_unsupported_compression'] = 'Le format de compression que vous avez choisi n\'est pas support&eacute; par votre serveur.';
