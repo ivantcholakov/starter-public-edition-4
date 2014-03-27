c8bd7638fffb5a341b65dee8ea170bbce98dd29e
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-03-27 12:58:33 +0200

    Adding language support for DataTables jQuery plug-in.

---
+++ platform/applications/site_example/core/MY_Lang.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Lang extends Core_Lang {
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+    public function datatables($language = null) {
+
+        if ($language == '') {
+            $language = $this->current();
+        }
+
+        $lang = @ file_get_contents(DEFAULTFCPATH.'assets/js/lib/dataTables/i18n/'.$language.'.lang', false);
+
+        if ($lang == '') {
+            return '{}';
+        }
+
+        $lang = @json_decode($lang);
+
+        if ($lang == '') {
+            return '{}';
+        }
+
+        return json_encode($lang);
+    }
+
+}
