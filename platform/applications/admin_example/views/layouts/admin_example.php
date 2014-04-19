93a4eb1785b3583c98d67a12b44cefca0bc3bb4b
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-04-19 22:11:10 +0300

    Updating Jasny Bootstrap, 3.1.1 -> 3.1.2.

--- platform/applications/admin_example/views/layouts/admin_example.php
+++ platform/applications/admin_example/views/layouts/admin_example.php
@@ -17,7 +17,6 @@ echo cleartype_ie();
 
 echo css('lib/bootstrap-3/bootstrap.min.css');
 echo css('lib/jasny-bootstrap-3/jasny-bootstrap.min.css');
-echo css('lib/jasny-bootstrap-3/jasny-bootstrap_3.1.1_patch.css');
 echo css('lib/bootstrap-3/bootstrap-theme.min.css');
 echo css('lib/font-awesome-4/font-awesome.min.css');
 echo css('admin_example.css');
