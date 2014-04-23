76c9f52bcfec3b549903d3ee63a0a4bcc44c5e03
Gwenael G <gwenael.gallon@mac.com>
2014-04-23 08:54:59 +0200

    Update French language

--- platform/core/common/language/french/ui_lang.php
+++ platform/core/common/language/french/ui_lang.php
@@ -130,5 +130,5 @@ $lang['ui_updated_by'] = 'Modifier par';
 $lang['ui_deleted'] = 'Supprimer';
 $lang['ui_deleted_at'] = 'Supprimer le';
 $lang['ui_deleted_by'] = 'Supprimer par';
-$lang['ui_missing_language_identificator'] = 'Language identificator is missing.';
-$lang['ui_invalid_language_identificator'] = 'Language identificator is invalid.';
+$lang['ui_missing_language_identificator'] = 'L\'identifiant de langue est manquant.';
+$lang['ui_invalid_language_identificator'] = 'L\'identifiant de langue est invalide.';
