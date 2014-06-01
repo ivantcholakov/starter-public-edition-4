0fa928c1472e761b5fdc9ae3476174b6d8a5a66f
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-06-01 13:14:17 +0300

    Updating CodeIgniter.

--- platform/core/framework/libraries/Form_validation.php
+++ platform/core/framework/libraries/Form_validation.php
@@ -1225,6 +1225,11 @@ class CI_Form_validation {
 	 */
 	public function valid_email($str)
 	{
+		if (function_exists('idn_to_ascii') && $atpos = strpos($str, '@'))
+		{
+			$str = substr($str, 0, ++$atpos).idn_to_ascii(substr($str, $atpos));
+		}
+
 		return (bool) filter_var($str, FILTER_VALIDATE_EMAIL);
 	}
 
