8c486a62683ffb467777688f7e0a1e0cff516cf9
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-06-05 19:18:04 +0300

    Adding a setting and a library for symmetric password encryption.

---
+++ platform/core/common/libraries/Password.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+
+/**
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2014
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ */
+
+class Password {
+
+    protected $key;
+
+    public function __construct() {
+
+        $this->key = config_item('encryption_key_for_passwords');
+
+        log_message('debug', 'Password class initialized');
+    }
+
+    public function encrypt($password) {
+
+        $password = (string) $password;
+
+        if ($password == '') {
+            return '';
+        }
+
+        $old_key_size = GibberishAES::size();
+        GibberishAES::size(256);
+
+        $result = GibberishAES::enc($password, $this->key);
+
+        GibberishAES::size($old_key_size);
+
+        return $result;
+    }
+
+    public function decrypt($password) {
+
+        $password = (string) $password;
+
+        if ($password == '') {
+            return '';
+        }
+
+        $old_key_size = GibberishAES::size();
+        GibberishAES::size(256);
+
+        $result = GibberishAES::dec($password, $this->key);
+
+        GibberishAES::size($old_key_size);
+
+        return $result;
+    }
+
+}
