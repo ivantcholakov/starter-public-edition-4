a923bc8706307d720e8ff4009c36f9a6f4280c0f
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-06-14 03:05:03 +0300

    An insignificant correction.

--- platform/applications/site_example/modules/welcome/views/welcome_message.php
+++ platform/applications/site_example/modules/welcome/views/welcome_message.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
             <div class="page-header">
-                <h1 class="my_section">Application Starter 4 Public Edition</h1>
+                <h1>Application Starter 4 Public Edition</h1>
             </div>
 
             <p>
