0d71250a6bcfa7e1542d6f03434b7473d79ab5c5
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-07-30 03:05:50 +0300

    A update and corrections in Core_Model class (BC is kept).

--- platform/core/common/core/Core_Model.php
+++ platform/core/common/core/Core_Model.php
@@ -156,7 +156,8 @@ class Core_Model extends CI_Model
     /**
      * Additional scope that enforces JSON presentation of the returned result.
      */
-    protected $_as_json = NULL;
+    protected $_as_json = FALSE;
+    protected $_as_json_options = 0;
 
     /**
      * A flag indicating $this->distinct() usage.
@@ -313,14 +314,12 @@ class Core_Model extends CI_Model
             return $this->_return_value($row);
         }
 
-        $as_json = $this->_as_json;
+        if ($this->_as_json)
+        {
+            return $this->_return_json($row);
+        }
 
         $this->_reset_state();
-
-        if ($as_json)
-        {
-            return json_encode($row);
-        }
 
         return $row;
     }
@@ -394,14 +393,12 @@ class Core_Model extends CI_Model
             $row = $this->trigger('after_get', $row, ($key == count($result) - 1));
         }
 
-        $as_json = $this->_as_json;
+        if ($this->_as_json)
+        {
+            return $this->_return_json($result);
+        }
 
         $this->_reset_state();
-
-        if ($as_json)
-        {
-            return json_encode($result);
-        }
 
         return $result;
     }
@@ -437,6 +434,8 @@ class Core_Model extends CI_Model
 
             $this->trigger('after_create', $insert_id);
 
+            $this->_reset_state();
+
             return $insert_id;
         }
 
@@ -451,12 +450,14 @@ class Core_Model extends CI_Model
     public function insert_many($data, $skip_validation = FALSE, $escape = NULL)
     {
         $return_sql = $this->qb_as_sql;
+        $skip_observers = $this->_temporary_skip_observers;
 
         $ids = array();
 
         foreach ($data as $key => $row)
         {
             $this->qb_as_sql = $return_sql;
+            $this->_temporary_skip_observers = $skip_observers;
 
             // A correction by Ivan Tcholakov, 14-DEC-2012.
             //$ids[] = $this->insert($row, $skip_validation, ($key == count($data) - 1));
@@ -986,14 +987,12 @@ class Core_Model extends CI_Model
 
         $result = isset($row[$this->primary_key]);
 
-        $as_json = $this->_as_json;
+        if ($this->_as_json)
+        {
+            return $this->_return_json($result);
+        }
 
         $this->_reset_state();
-
-        if ($as_json)
-        {
-            return json_encode($result);
-        }
 
         return $result;
     }
@@ -1016,14 +1015,12 @@ class Core_Model extends CI_Model
 
         $row = $this->trigger('after_get', $row);
 
-        $as_json = $this->_as_json;
+        if ($this->_as_json)
+        {
+            return $this->_return_json($row);
+        }
 
         $this->_reset_state();
-
-        if ($as_json)
-        {
-            return json_encode($row);
-        }
 
         return $row;
     }
@@ -1078,14 +1075,12 @@ class Core_Model extends CI_Model
 
         $options = $this->trigger('after_dropdown', $options);
 
-        $as_json = $this->_as_json;
+        if ($this->_as_json)
+        {
+            return $this->_return_json($options);
+        }
 
         $this->_reset_state();
-
-        if ($as_json)
-        {
-            return json_encode($options);
-        }
 
         return $options;
     }
@@ -1146,14 +1141,12 @@ class Core_Model extends CI_Model
 
         $result = $this->_database->count_all_results($this->_table);
 
-        $as_json = $this->_as_json;
+        if ($this->_as_json)
+        {
+            return $this->_return_json($result);
+        }
 
         $this->_reset_state();
-
-        if ($as_json)
-        {
-            return json_encode($result);
-        }
 
         return $result;
     }
@@ -1325,10 +1318,14 @@ class Core_Model extends CI_Model
 
     /**
      * Forces returning JSON encoded data as a result.
+     * @param int $options  Same parameter as in json_encode() function (PHP >= 5.3.0)
+     * @link http://php.net/manual/en/function.json-encode.php
+     * @link http://php.net/manual/en/json.constants.php
      */
-    public function as_json()
+    public function as_json($options = 0)
     {
         $this->_as_json = TRUE;
+        $this->_as_json_options = $options;
         return $this;
     }
 
@@ -2094,10 +2091,6 @@ class Core_Model extends CI_Model
      */
     protected function _return_value(& $row)
     {
-        $as_json = $this->_as_json;
-
-        $this->_reset_state();
-
         $result = NULL;
 
         if (is_array($row))
@@ -2117,12 +2110,23 @@ class Core_Model extends CI_Model
             }
         }
 
-        if ($as_json)
+        if ($this->_as_json)
         {
-            return json_encode($result);
+            return $this->_return_json($result);
         }
 
+        $this->_reset_state();
+
         return $result;
+    }
+
+    protected function _return_json(& $data)
+    {
+        $as_json_options = $this->_as_json_options;
+
+        $this->_reset_state();
+
+        return is_php('5.3.0') ? json_encode($data, $as_json_options) : json_encode($data);
     }
 
     /**
@@ -2197,6 +2201,7 @@ class Core_Model extends CI_Model
         $this->qb_as_sql = FALSE;
         $this->qb_distinct = FALSE;
         $this->_as_json = FALSE;
+        $this->_as_json_options = 0;
         $this->_temporary_skip_observers = FALSE;
     }
 
@@ -2208,8 +2213,8 @@ class Core_Model extends CI_Model
         if (is_callable($this->user_id_getter))
         {
             return is_array($this->user_id_getter)
-                    ? $this->user_id_getter[0]->{$this->user_id_getter[1]}()
-                    : call_user_func($this->user_id_getter);
+                ? $this->user_id_getter[0]->{$this->user_id_getter[1]}()
+                : call_user_func($this->user_id_getter);
         }
 
         return NULL;
