0b193db5ebd5d1f086234d3c44938feb6e0c41ec
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-08-06 05:33:07 +0300

    The admin panel: Adding a controller for nested HMVC triads - "widgets".

---
+++ platform/applications/admin_example/core/Base_Widget_Controller.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Base_Widget_Controller extends Core_Controller {
+
+    public function __construct() {
+
+        parent::__construct();
+
+        $this->load
+            ->helper('asset')
+        ;
+
+    }
+
+    public function _remap() {
+
+        show_404();
+    }
+
+}
