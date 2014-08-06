12a7af151a6399ceee0088e7e37bb81bc954c41f
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-08-06 07:00:53 +0300

    A Bulgarian language correction.

--- platform/core/common/language/bulgarian/ui_lang.php
+++ platform/core/common/language/bulgarian/ui_lang.php
@@ -149,5 +149,5 @@ $lang['ui_height'] = 'Височина';
 $lang['ui_max_width'] = 'Широчина, max';
 $lang['ui_max_height'] = 'Височина, max';
 $lang['ui_options'] = 'Опции';
-$lang['ui_session_expired'] = 'Вашата сесия изтече, моля, влезте в системата отново.';
+$lang['ui_session_expired'] = 'Вашата сесия e изтекла, моля, влезте в системата отново.';
 $lang['ui_admin_panel'] = 'Административен панел';
