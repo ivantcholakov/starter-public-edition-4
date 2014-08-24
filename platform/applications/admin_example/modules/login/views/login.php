9ca2f5540125efee8a57fb6bcfaacc683ea46aa9
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-08-24 18:43:48 +0300

    A minor visual correction within admin login page.

--- platform/applications/admin_example/modules/login/views/login.php
+++ platform/applications/admin_example/modules/login/views/login.php
@@ -2,15 +2,19 @@
 
             <?php echo form_open('', 'class="form-signin" role="form"'); ?>
 
+                <h2 class="form-signin-heading">Login Page</h2>
+
 <?php
+
 if ($error_message != '') {
 
 ?>
+
                 <div class="alert alert-danger"><?php echo $error_message; ?></div>
 <?php
 }
 
-?>              <h2 class="form-signin-heading">Login Page</h2>
+?>
 
                 <input type="text" name="username" id="username" class="form-control" placeholder="Username" value="<?php echo set_value('username'); ?>" />
                 <input type="password" name="password" id="password" class="form-control" placeholder="Password" value="<?php echo set_value('password'); ?>" />
