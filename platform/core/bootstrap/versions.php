76598db4f8337c6cf352327e551db8498a67229b
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-09-26 21:20:04 +0300

    Tagging a new releare candidate v4.0.0-rc18.

--- platform/core/bootstrap/versions.php
+++ platform/core/bootstrap/versions.php
@@ -4,11 +4,11 @@
  * CodeIgniter Version
  * @var    string
  */
- define('CI_VERSION', '3.0-dev');
+define('CI_VERSION', '3.0-dev');
 
 
- /**
+/**
  * Platform (Application Starter) Version
  * @var    string
  */
- define('PLATFORM_VERSION', '4.0.0-rc17');
+define('PLATFORM_VERSION', '4.0.0-rc18');
