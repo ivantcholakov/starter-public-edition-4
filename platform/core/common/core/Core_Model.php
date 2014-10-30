175d5703faf9691d7fa30758cd5e5f62e65ebc20
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-10-30 14:47:13 +0200

    Core_Model: The wrapper method reset_query() has been added.

--- platform/core/common/core/Core_Model.php
+++ platform/core/common/core/Core_Model.php
@@ -1974,6 +1974,18 @@ class Core_Model extends CI_Model
         return $this->_database->table_exists($table_name);
     }
 
+    /**
+     * A wrapper to CodeIgniter 3 $this->_database->reset_query()
+     */
+    public function reset_query() {
+
+        if ($this->_is_ci_3) {
+            $this->_database->reset_query();
+        }
+
+        $this->_reset_state();
+    }
+
     /* --------------------------------------------------------------
      * I18N
      * ------------------------------------------------------------ */
