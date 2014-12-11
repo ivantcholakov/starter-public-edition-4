decb4c863a80d860fa5f301e1f30d7ad1e3c6148
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-12-11 09:03:14 +0200

    A tiny change about showing the copyright note.

--- platform/applications/site_example/views/partials/site_example_footer.php
+++ platform/applications/site_example/views/partials/site_example_footer.php
@@ -2,7 +2,7 @@
 
     <footer id="footer">
         <div class="container">
-            <p class="text-muted pull-left"><?php echo str_replace('{year}', date('Y'), $this->settings->lang('copyright_note')); ?></p>
+            <p class="text-muted pull-left"><?php echo str_replace(array('{year}', '{organization}'), array(date('Y'), $this->settings->lang('contact_organization')), $this->settings->lang('copyright_note')); ?></p>
             <a id="scroll-top" href="<?php echo CURRENT_URI; ?>#page-top" title="<?php echo lang('ui_go_to_top'); ?>"><i class="fa fa-chevron-up"></i></a>
         </div>
     </footer>
