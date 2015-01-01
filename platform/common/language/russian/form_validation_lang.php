36729bdb40624a7c51e2161cbb5a0c8a57880e79
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-01-01 09:50:58 +0200

    Updating some language files.

--- platform/common/language/russian/form_validation_lang.php
+++ platform/common/language/russian/form_validation_lang.php
@@ -1,34 +1,69 @@
 <?php
 /**
- * @author
- * @license     The MIT License (MIT), http://opensource.org/licenses/MIT
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['form_validation_required'] = 'The {field} field is required.';
-$lang['form_validation_isset'] = 'The {field} field must have a value.';
-$lang['form_validation_valid_email'] = 'The {field} field must contain a valid email address.';
-$lang['form_validation_valid_emails'] = 'The {field} field must contain all valid email addresses.';
-$lang['form_validation_valid_url'] = 'The {field} field must contain a valid URL.';
-$lang['form_validation_valid_ip'] = 'The {field} field must contain a valid IP.';
-$lang['form_validation_min_length'] = 'The {field} field must be at least {param} characters in length.';
-$lang['form_validation_max_length'] = 'The {field} field cannot exceed {param} characters in length.';
-$lang['form_validation_exact_length'] = 'The {field} field must be exactly {param} characters in length.';
-$lang['form_validation_alpha'] = 'The {field} field may only contain alphabetical characters.';
-$lang['form_validation_alpha_numeric'] = 'The {field} field may only contain alpha-numeric characters.';
-$lang['form_validation_alpha_numeric_spaces'] = 'The {field} field may only contain alpha-numeric characters and spaces.';
-$lang['form_validation_alpha_dash'] = 'The {field} field may only contain alpha-numeric characters, underscores, and dashes.';
-$lang['form_validation_numeric'] = 'The {field} field must contain only numbers.';
-$lang['form_validation_is_numeric'] = 'The {field} field must contain only numeric characters.';
-$lang['form_validation_integer'] = 'The {field} field must contain an integer.';
-$lang['form_validation_regex_match'] = 'The {field} field is not in the correct format.';
-$lang['form_validation_matches'] = 'The {field} field does not match the {param} field.';
-$lang['form_validation_differs'] = 'The {field} field must differ from the {param} field.';
-$lang['form_validation_is_unique']  = 'The {field} field must contain a unique value.';
-$lang['form_validation_is_natural'] = 'The {field} field must only contain digits.';
-$lang['form_validation_is_natural_no_zero'] = 'The {field} field must only contain digits and must be greater than zero.';
-$lang['form_validation_decimal'] = 'The {field} field must contain a decimal number.';
-$lang['form_validation_less_than'] = 'The {field} field must contain a number less than {param}.';
-$lang['form_validation_less_than_equal_to'] = 'The {field} field must contain a number less than or equal to {param}.';
-$lang['form_validation_greater_than'] = 'The {field} field must contain a number greater than {param}.';
-$lang['form_validation_greater_than_equal_to'] = 'The {field} field must contain a number greater than or equal to {param}.';
+$lang['form_validation_required'] = 'Поле {field} обязательно.';
+$lang['form_validation_isset'] = 'Поле {field} должно быть заполнено.';
+$lang['form_validation_valid_email'] = 'Поле {field} должно содержать правильный E-mail адрес.';
+$lang['form_validation_valid_emails'] = 'Поле {field} должно содержать правильные E-mail адреса.';
+$lang['form_validation_valid_url'] = 'Поле {field} должно содержать правильный URL.';
+$lang['form_validation_valid_ip'] = 'Поле {field} должно содержать правильный IP.';
+$lang['form_validation_min_length'] = 'Длина поля {field} должна быть по крайней мере {param} символов.';
+$lang['form_validation_max_length'] = 'Длина поля {field} не может превышать {param} символов.';
+$lang['form_validation_exact_length'] = 'Длина поля {field} должна быть равной {param} символов.';
+$lang['form_validation_alpha'] = 'Поле {field} может состоять только из букв.';
+$lang['form_validation_alpha_numeric'] = 'Поле {field} может состоять только из букв и цифр.';
+$lang['form_validation_alpha_numeric_spaces'] = 'Поле {field} может состоять только из букв, цифр или пробелов.';
+$lang['form_validation_alpha_dash'] = 'Поле {field} может состоять только из букв, цифр, знаков подчеркивания и тире.';
+$lang['form_validation_numeric'] = 'Поле {field} может состоять только из цифр.';
+$lang['form_validation_is_numeric'] = 'Поле {field} может состоять только из цифровых значений.';
+$lang['form_validation_integer'] = 'Поле {field} должно содержать целочисленное значение.';
+$lang['form_validation_regex_match'] = 'Поле {field} заполнено неправильно.';
+$lang['form_validation_matches'] = 'Поле {field} не соответствует параметру {param}.';
+$lang['form_validation_differs'] = 'Поле {field} должно отличаться от параметра {param}.';
+$lang['form_validation_is_unique'] = 'Поле {field} должно быть уникальным.';
+$lang['form_validation_is_natural'] = 'Поле {field} должно содержать только цифры.';
+$lang['form_validation_is_natural_no_zero'] = 'Поле {field} должно содержать только цифры и быть больше нуля.';
+$lang['form_validation_decimal'] = 'Поле {field} должно содержать десятичное значение.';
+$lang['form_validation_less_than'] = 'Поле {field} должно содержать значение меньше {param}.';
+$lang['form_validation_less_than_equal_to'] = 'Поле {field} должно содержать значение меньше или равно {param}.';
+$lang['form_validation_greater_than'] = 'Поле {field} должно содержать значение больше {param}.';
+$lang['form_validation_greater_than_equal_to'] = 'Поле {field} должно содержать значение больше или равно {param}.';
+
+/* End of file form_validation_lang.php */
+/* Location: ./application/language/russian/form_validation_lang.php */
