e5099039863cf4393b45a66690f21ce456437649
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-01-15 12:58:03 +0200

    Corrections in some Polish files.

--- platform/common/language/polish/welcome_lang.php
+++ platform/common/language/polish/welcome_lang.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author      Ivan Tcholakov <ivantcholakov@gmail.com>, 2014.
+ * @author
  * @license     The MIT License (MIT), http://opensource.org/licenses/MIT
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
