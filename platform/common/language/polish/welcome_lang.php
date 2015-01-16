5d97b5afd55f37b1d7a1ca6ee39e450079c75e3b
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-01-16 17:14:32 +0200

    Polish translations - author.

--- platform/common/language/polish/welcome_lang.php
+++ platform/common/language/polish/welcome_lang.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author
+ * @author      Exelord (GitHub name), 2015.
  * @license     The MIT License (MIT), http://opensource.org/licenses/MIT
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
