7cce4841bfc54bc2183570b3a5b7f43c3103a86f
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-01-21 01:51:36 +0200

    Tagging a new release candidate v4.0.0-rc64.

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -11,4 +11,4 @@ define('CI_VERSION', '3.0-dev');
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.0.0-rc63');
+define('PLATFORM_VERSION', '4.0.0-rc64');
