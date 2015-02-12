a2169fee04e83c79ba0d89541296ead3a2585a44
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-02-12 08:25:09 +0200

    Suppressing warnings within session 'files' drives.

--- platform/system/libraries/Session/drivers/Session_files_driver.php
+++ platform/system/libraries/Session/drivers/Session_files_driver.php
@@ -299,13 +299,13 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	{
 		if ($this->close())
 		{
-			return unlink($this->_file_path.$session_id) && $this->_cookie_destroy();
+			return @ unlink($this->_file_path.$session_id) && $this->_cookie_destroy();
 		}
 		elseif ($this->_file_path !== NULL)
 		{
 			clearstatcache();
 			return file_exists($this->_file_path.$session_id)
-				? (unlink($this->_file_path.$session_id) && $this->_cookie_destroy())
+				? (@ unlink($this->_file_path.$session_id) && $this->_cookie_destroy())
 				: TRUE;
 		}
 
