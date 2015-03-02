2587bce8ec915348557c95619b1269a60168ceb3
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-03-02 04:22:34 +0200

    Updating CKEditor.

--- platform/common/classes/CKEditor.php
+++ platform/common/classes/CKEditor.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '4.4.6';
+	const version = '4.4.7';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'D13H8XS';
+	const timestamp = 'C32JNLW';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "D13H8XS";
+	public $timestamp = "C32JNLW";
 	/**
 	 * An array that holds event listeners.
 	 */
