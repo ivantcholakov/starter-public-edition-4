1fe35aeb955dbe3235c072512ade44f0a4dd5112
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-03-12 11:27:54 +0200

    Tagging a new release candidate v4.0.0-rc97. CodeIgniter version is 3.0rc3 now.

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -4,11 +4,11 @@
  * CodeIgniter Version
  * @var    string
  */
-define('CI_VERSION', '3.0rc2');
+define('CI_VERSION', '3.0rc3');
 
 
 /**
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.0.0-rc96');
+define('PLATFORM_VERSION', '4.0.0-rc97');
