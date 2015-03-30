009941a9b0d8cc4f4abfd75d88770ee61b572d40
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-03-31 01:15:49 +0300

    Adding Romanian language files.

---
+++ platform/common/language/romanian/mailer_lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author
+ * @license     The MIT License (MIT), http://opensource.org/licenses/MIT
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['mailer_disabled'] = 'E-mail service has not been activated.';
+$lang['mailer_disabled_error'] = 'We are sorry, e-mail service has not been activated. The message has not been sent.';
+$lang['mailer_error'] = 'We are sorry, your message has not been sent due to a technical problem.';
+$lang['mailer_a_message_has_been_received'] = 'A message has been received';
+$lang['mailer_a_message_has_been_received_from'] = 'A message from';
+$lang['mailer_your_message_has_been_sent'] = "Thank you! Your message has been sent.";
+$lang['mailer_confirmation_for_a_received_message'] = 'Confirmation for a received message';
+$lang['mailer_confirmation_for_a_received_message_from'] = 'Confirmation for a received message from';
