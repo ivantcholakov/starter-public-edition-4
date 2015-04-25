a2284ad2f80b10edda7ebd26f66f473bf7d4c4aa
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-04-25 22:21:23 +0300

    An insignificant change.

--- platform/common/config/less_compile.php
+++ platform/common/config/less_compile.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2013
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2013-2015
  * @license The MIT License, http://opensource.org/licenses/MIT
  */
 
