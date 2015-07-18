c7adeaf3551e27f1bbc20cacfad4b380df2e439b
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-18 11:39:08 +0300

    Insignificant changes.

--- platform/common/config/database.php
+++ platform/common/config/database.php
@@ -32,12 +32,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |    ['char_set'] The character set used in communicating with the database
 |    ['dbcollat'] The character collation used in communicating with the database
 |                 NOTE: For MySQL and MySQLi databases, this setting is only used
-|                  as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
+|                 as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
 |                 (and in table creation queries made with DB Forge).
-|                  There is an incompatibility in PHP with mysql_real_escape_string() which
-|                  can make your site vulnerable to SQL injection if you are using a
-|                  multi-byte character set and are running versions lower than these.
-|                  Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
+|                 There is an incompatibility in PHP with mysql_real_escape_string() which
+|                 can make your site vulnerable to SQL injection if you are using a
+|                 multi-byte character set and are running versions lower than these.
+|                 Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
 |    ['swap_pre'] A default table prefix that should be swapped with the dbprefix
 |    ['encrypt']  Whether or not to use an encrypted connection.
 |
