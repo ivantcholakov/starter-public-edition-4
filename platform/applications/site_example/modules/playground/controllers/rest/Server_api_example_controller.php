c81908f633dc56082ee465ec4d3b5d066ee7d37d
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-22 04:30:06 +0300

    REST server: Updating examples.

--- platform/applications/site_example/modules/playground/controllers/rest/Server_api_example_controller.php
+++ platform/applications/site_example/modules/playground/controllers/rest/Server_api_example_controller.php
@@ -27,9 +27,16 @@ class Server_api_example_controller extends REST_Controller {
         $this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
-    public function user_get()
+    public function user_get($id)
     {
-        if (!$this->get('id'))
+        if ($this->get('id') !== NULL)
+        {
+            $id = $this->get('id');
+        }
+
+        $id = (int) $id;
+
+        if ($id <= 0)
         {
             // Set the response and exit
             $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
@@ -42,12 +49,8 @@ class Server_api_example_controller extends REST_Controller {
             3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => 'Is a Scott!', array('hobbies' => array('fartings', 'bikes'))),
         );
 
-        // Get the user from the array, by retrieving the id from the GET request
-        // Modified by Ivan Tcholakov, 21-JUL-2015.
-        //$user = isset($users[$this->get('id')]) ? $users[$this->get('id')] : NULL;
-        $id = (int) $this->get('id');
+        // Get the user from the array by the given id.
         $user = isset($users[$id]) ? $users[$id] : NULL;
-        //
 
         if ($user)
         {
--- platform/applications/site_example/modules/playground/views/rest/server.php
+++ platform/applications/site_example/modules/playground/views/rest/server.php
@@ -39,10 +39,16 @@ template_partial('subnavbar');
                         <ul>
                             <li><a href="<?php echo site_url('playground/rest/server-api-example/users'); ?>">Users</a> - defaulting to JSON</li>
                             <li><a href="<?php echo site_url('playground/rest/server-api-example/users/format/csv'); ?>">Users</a> - get it in CSV</li>
-                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/id/1'); ?>">User #1</a> - defaulting to JSON</li>
-                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/id/1/format/xml'); ?>">User #1</a> - get it in XML</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/id/1'); ?>">User #1</a> - defaulting to JSON  (user/id/1)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/1'); ?>">User #1</a> - defaulting to JSON  (user/1)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/id/1.xml'); ?>">User #1</a> - get it in XML (user/id/1.xml)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/id/1/format/xml'); ?>">User #1</a> - get it in XML (user/id/1/format/xml)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/id/1?format=xml'); ?>">User #1</a> - get it in XML (user/id/1?format=xml)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/user/1.xml'); ?>">User #1</a> - get it in XML (user/1.xml)</li>
                             <li><a id="ajax" href="<?php echo site_url('playground/rest/server-api-example/users/format/json'); ?>">Users</a> - get it in JSON (AJAX request)</li>
-                            <li><a href="<?php echo site_url('playground/rest/server-api-example/users/format/html'); ?>">Users</a> - get it in HTML</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/users.html'); ?>">Users</a> - get it in HTML (users.html)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/users/format/html'); ?>">Users</a> - get it in HTML (users/format/html)</li>
+                            <li><a href="<?php echo site_url('playground/rest/server-api-example/users?format=html'); ?>">Users</a> - get it in HTML (users?format=html)</li>
                             <li><a href="<?php echo site_url('playground/rest/server-api-example/users/format/debug'); ?>">Users</a> - get it as a debugging HTML preview</li>
                         </ul>
 
