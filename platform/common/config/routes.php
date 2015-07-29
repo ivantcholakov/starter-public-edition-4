78702d587bb301c9e1c96bb8e0df693d4ebe0f20
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-29 14:21:26 +0300

    An insignificant change.

--- platform/common/config/routes.php
+++ platform/common/config/routes.php
@@ -46,8 +46,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | When you set this option to TRUE, it will replace ALL dashes in the
 | controller and method URI segments.
 |
-| Examples:	my-controller/index -> my_controller/index
-|       my-controller/my-method -> my_controller/my_method
+| Examples:	my-controller/index     -> my_controller/index
+|               my-controller/my-method -> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
