dbd4474033e79c592a7d529023c15069b3a20dac
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-08-21 13:17:35 +0300

    Backporting changes from CodeIgniter 3.0.2-dev.

--- platform/common/third_party/MX/Loader.php
+++ platform/common/third_party/MX/Loader.php
@@ -576,7 +576,7 @@ class MX_Loader extends CI_Loader
             load_class('Model', 'core');
         }
 
-        $model = ucfirst(strtolower($model));
+        $model = ucfirst($model);
 
         foreach ($this->_ci_model_paths as $mod_path)
         {
