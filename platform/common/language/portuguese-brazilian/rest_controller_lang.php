b8a030b59dd13a848b084a41b23addb535d94c9f
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-09-28 21:01:10 +0300

    REST Controller: Adding Brazilian Portuguese language.

--- platform/common/language/portuguese-brazilian/rest_controller_lang.php
+++ platform/common/language/portuguese-brazilian/rest_controller_lang.php
@@ -1,17 +1,17 @@
 <?php
 
 /*
-* Brazilian Portuguese language
-*/
+ * Brazilian portuguese language
+ */
 
-$lang['text_rest_invalid_api_key'] = 'Invalid API key %s'; // %s is the REST API key
-$lang['text_rest_invalid_credentials'] = 'Invalid credentials';
-$lang['text_rest_ip_denied'] = 'IP denied';
-$lang['text_rest_ip_unauthorized'] = 'IP unauthorized';
-$lang['text_rest_unauthorized'] = 'Unauthorized';
-$lang['text_rest_ajax_only'] = 'Only AJAX requests are allowed';
-$lang['text_rest_api_key_unauthorized'] = 'This API key does not have access to the requested controller';
-$lang['text_rest_api_key_permissions'] = 'This API key does not have enough permissions';
-$lang['text_rest_api_key_time_limit'] = 'This API key has reached the time limit for this method';
-$lang['text_rest_unknown_method'] = 'Unknown method';
-$lang['text_rest_unsupported'] = 'Unsupported protocol';
+$lang['text_rest_invalid_api_key'] = 'Chave da API %s inválida'; // %s is the REST API key
+$lang['text_rest_invalid_credentials'] = 'Credenciais inválidas';
+$lang['text_rest_ip_denied'] = 'IP proibido';
+$lang['text_rest_ip_unauthorized'] = 'IP não autorizado';
+$lang['text_rest_unauthorized'] = 'Não autorizado';
+$lang['text_rest_ajax_only'] = 'Apenas chamadas AJAX são permitidas';
+$lang['text_rest_api_key_unauthorized'] = 'Esta chave da API não tem acesso ao controller solicitado';
+$lang['text_rest_api_key_permissions'] = 'Esta chave da API não tem permissões suficientes';
+$lang['text_rest_api_key_time_limit'] = 'Esta chave da API já atingiu o tempo limite para este método';
+$lang['text_rest_unknown_method'] = 'Método desconhecido';
+$lang['text_rest_unsupported'] = 'Sem suporte para este protocolo';
