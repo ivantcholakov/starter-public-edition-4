d59173fcc4b376894cac27d243c7a2bb410d1f96
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-11-01 02:13:37 +0200

    $_SERVER['HTTP_HOST'] is not tried to be used for URL autodetection anymore.

--- platform/bootstrap/detect_host.php
+++ platform/bootstrap/detect_host.php
@@ -2,7 +2,7 @@
 
 /**
  * Server Name Detection Routine
- * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2013
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2013-2015
  * @license The MIT License, http://opensource.org/licenses/MIT
  */
 
@@ -14,11 +14,8 @@ if (!function_exists('detect_host')) {
 
             isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME']
                 : (isset($_SERVER['HOSTNAME']) ? $_SERVER['HOSTNAME']
-                // The regular expression is only a basic validation for a valid "Host" header.
-                // It's not exhaustive, only checks for valid characters.
-                : (isset($_SERVER['HTTP_HOST']) && preg_match('/^((\[[0-9a-f:]+\])|(\d{1,3}(\.\d{1,3}){3})|[a-z0-9\-\.]+)(:\d+)?$/i', $_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST']
                 : (isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR']
-                : 'localhost')));
+                : 'localhost'));
     }
 
 }
