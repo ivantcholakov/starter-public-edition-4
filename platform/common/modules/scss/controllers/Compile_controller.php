8e22dc733396636062a6b642a4b3f612d26b1f71
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-11-26 06:56:13 +0200

    Adding a command-line SCSS compiler.

---
+++ platform/common/modules/scss/controllers/Compile_controller.php
@@ -0,0 +1,70 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * SCSS Assets Compiler
+ *
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2015
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ */
+
+class Compile_controller extends Core_Controller {
+
+    protected $items;
+
+    public function __construct() {
+
+        parent::__construct();
+
+        if (!IS_CLI) {
+            show_404();
+        }
+
+        $this->config->load('scss_compile', false, true);
+
+        $this->items
+            = !empty($this->config->config['scss_compile']) && is_array($this->config->config['scss_compile'])
+            ? $this->config->config['scss_compile']
+            : array();
+
+        $this->load->parser('scss');
+        $this->load->helper('file');
+    }
+
+    public function index() {
+
+        $args = array_slice($this->uri->rsegment_array(), 2);
+
+        foreach ($this->items as $options) {
+
+            $name = isset($options['name']) ? (string) $options['name'] : null;
+
+            if (!empty($args)) {
+
+                if (!in_array($name, $args)) {
+                    continue;
+                }
+            }
+
+            $source = isset($options['source']) ? (string) $options['source'] : '';
+            $destination = isset($options['destination']) ? (string) $options['destination'] : '';
+
+            if ($source == '' || $destination == '') {
+                continue;
+            }
+
+            unset($options['source']);
+            unset($options['destination']);
+            $options['full_path'] = true;
+
+            $dir = pathinfo($destination, PATHINFO_DIRNAME);
+            file_exists($dir) OR mkdir($dir, 0755, TRUE);
+
+            try {
+                write_file($destination, $this->scss->parse($source, null, true, $options));
+            } catch(Exception $e) {
+                echo $e->getMessage().PHP_EOL;
+            }
+        }
+    }
+
+}
