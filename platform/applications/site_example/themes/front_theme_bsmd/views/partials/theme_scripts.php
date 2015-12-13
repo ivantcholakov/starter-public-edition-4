9f259d9deba2b7163242497ff4dea17f3bf38023
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-12-13 21:58:55 +0200

    Bootstrap Material Design theme - adding some of the JavaScripts.

---
+++ platform/applications/site_example/themes/front_theme_bsmd/views/partials/theme_scripts.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+echo js('lib/bootstrap-material-design/material.min.js');
+
+?>
+
+    <script type="text/javascript">
+    //<![CDATA[
+
+    $(function () {
+
+        $.material.init();
+    });
+
+    //]]>
+    </script>
