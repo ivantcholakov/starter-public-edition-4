358e3b94da697c933b6e0e415c81be948de3752a
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-12-16 16:55:18 +0200

    Trying the old Lex_Parser API.

--- platform/applications/site_example/modules/playground/views/lex_parser.php
+++ platform/applications/site_example/modules/playground/views/lex_parser.php
@@ -8,15 +8,24 @@
 $template = 'Hello, {{name}}';
 $data = array('name' => 'John');
 
+// Direct usage the Lex parser API.
 $parser = new Lex\Parser();
 echo $parser->parse($template, $data);
 
 echo '<br />';
 
+// Direct usage of the old Lex parser API (added for BC).
+$parser_2 = new Lex_Parser();
+echo $parser_2->parse($template, $data);
+
+echo '<br />';
+
+// Calling the Lex parser driver.
 echo $this->parser->parse_string($template, $data, true, 'lex');
 
 echo '<br />';
 
+// Applying the Lex parser on views.
 echo $this->load->view('test.lex', $data, true, 'lex');
 
 ?>
