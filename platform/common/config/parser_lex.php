5488c3597b7e4d3aca91cfbdbafa4aa15ac9bb55
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-12-16 06:58:59 +0200

    Adding a configuration file for the future Lex parser driver.

---
+++ platform/common/config/parser_lex.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2015
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ *
+ * @see https://github.com/pyrocms/lex
+ */
+
+// The character(s) used by Lex to trigger a scope change. A scope change is,
+// for instance accessing a nested variable inside and array/object,
+$config['scope_glue'] = '';
