e8ad31c1d1e7a4fc0b690fde1ec63e046dd8f337
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-02 19:47:56 +0200

    Lex Parser: Adding Asset and Url extensions, empty for now.

---
+++ platform/common/classes/Parser/Lex/Extension/Asset.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2016
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ */
+
+class Parser_Lex_Extension_Asset extends Parser_Lex_Extension {
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+}
