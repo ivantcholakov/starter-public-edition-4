9c193a62ea0c4267d1fa071e771659442d714857
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-04 10:39:35 +0200

    url:current uri:current url:uri_string url:query_string url:get

---
+++ platform/common/classes/Parser/Lex/Extension/Uri.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2016
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ */
+
+class Parser_Lex_Extension_Uri extends Parser_Lex_Extension {
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+    public function current() {
+
+        return CURRENT_URI;
+    }
+
+}
--- platform/common/classes/Parser/Lex/Extension/Url.php
+++ platform/common/classes/Parser/Lex/Extension/Url.php
@@ -10,6 +10,28 @@ class Parser_Lex_Extension_Url extends Parser_Lex_Extension {
     public function __construct() {
 
         parent::__construct();
+
+        $this->load->helper('url');
+    }
+
+    public function current() {
+
+        return CURRENT_URL;
+    }
+
+    public function uri_string() {
+
+        return CURRENT_URI_STRING;
+    }
+
+    public function query_string() {
+
+        return CURRENT_QUERY_STRING;
+    }
+
+    public function get() {
+
+        return $this->input->get($this->get_attribute(0));
     }
 
 }
