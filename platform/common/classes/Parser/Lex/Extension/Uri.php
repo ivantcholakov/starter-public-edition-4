5fb1916c0a633c079f8b680e962a9cb84f59c191
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-04 19:59:32 +0200

    url:get - a second parameter xss_clean has been added; url:site uri:site url:base uri:base

--- platform/common/classes/Parser/Lex/Extension/Uri.php
+++ platform/common/classes/Parser/Lex/Extension/Uri.php
@@ -10,11 +10,23 @@ class Parser_Lex_Extension_Uri extends Parser_Lex_Extension {
     public function __construct() {
 
         parent::__construct();
+
+        $this->load->helper('url');
     }
 
     public function current() {
 
         return CURRENT_URI;
+    }
+
+    public function site() {
+
+        return site_uri($this->get_attribute(0));
+    }
+
+    public function base() {
+
+        return base_uri($this->get_attribute(0));
     }
 
 }
--- platform/common/classes/Parser/Lex/Extension/Url.php
+++ platform/common/classes/Parser/Lex/Extension/Url.php
@@ -31,7 +31,19 @@ class Parser_Lex_Extension_Url extends Parser_Lex_Extension {
 
     public function get() {
 
-        return $this->input->get($this->get_attribute(0));
+        $this->detect_boolean_attributes(array(1));
+
+        return $this->input->get($this->get_attribute(0), (bool) $this->get_attribute(1));
+    }
+
+    public function site() {
+
+        return site_url($this->get_attribute(0));
+    }
+
+    public function base() {
+
+        return base_url($this->get_attribute(0));
     }
 
 }
