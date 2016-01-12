6ddafe64c25d370072604f7f03cef7e852761b6e
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-12 04:45:12 +0200

    Updating bootstrap3-dialog, v1.34.9.

--- www/assets/js/lib/bootstrap3-dialog/bootstrap-dialog.js
+++ www/assets/js/lib/bootstrap3-dialog/bootstrap-dialog.js
@@ -19,7 +19,7 @@
     if (typeof module !== 'undefined' && module.exports) {
         var isNode = (typeof process !== "undefined");
         var isElectron = isNode && ('electron' in process.versions);
-        if(isElectron) {
+        if (isElectron) {
             root.BootstrapDialog = factory(root.jQuery);
         } else {
             module.exports = factory(require('jquery'), require('bootstrap'));
@@ -849,12 +849,11 @@
                 var dialog = event.data.dialog;
                 var $button = event.data.$button;
                 var button = $button.data('button');
-                if (typeof button.action === 'function') {
-                    button.action.call($button, dialog, event);
-                }
-
                 if (button.autospin) {
                     $button.toggleSpin(true);
+                }
+                if (typeof button.action === 'function') {
+                    return button.action.call($button, dialog, event);
                 }
             });
 
@@ -862,8 +861,8 @@
             this.enhanceButton($button);
 
             //Initialize enabled or not
-            if(typeof button.enabled !== 'undefined') {
-              $button.toggleEnable(button.enabled);
+            if (typeof button.enabled !== 'undefined') {
+                $button.toggleEnable(button.enabled);
             }
 
             return $button;
@@ -1234,8 +1233,11 @@
                     label: options.buttonLabel,
                     action: function (dialog) {
                         dialog.setData('btnClicked', true);
-                        typeof dialog.getData('callback') === 'function' && dialog.getData('callback')(true);
-                        dialog.close();
+                        if (typeof dialog.getData('callback') === 'function' && dialog.getData('callback').call(this, true) === false) {
+                            return false;
+                        }
+
+                        return dialog.close();
                     }
                 }]
         }).open();
@@ -1285,15 +1287,21 @@
             buttons: [{
                     label: options.btnCancelLabel,
                     action: function (dialog) {
-                        typeof dialog.getData('callback') === 'function' && dialog.getData('callback')(false);
-                        dialog.close();
+                        if (typeof dialog.getData('callback') === 'function' && dialog.getData('callback').call(this, false) === false) {
+                            return false;
+                        }
+
+                        return dialog.close();
                     }
                 }, {
                     label: options.btnOKLabel,
                     cssClass: options.btnOKClass,
                     action: function (dialog) {
-                        typeof dialog.getData('callback') === 'function' && dialog.getData('callback')(true);
-                        dialog.close();
+                        if (typeof dialog.getData('callback') === 'function' && dialog.getData('callback').call(this, true) === false) {
+                            return false;
+                        }
+
+                        return dialog.close();
                     }
                 }]
         }).open();
