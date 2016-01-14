c26036c2668c9b5e383562f8b95f026db471db3b
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-14 05:15:19 +0200

    Configuration: Adding a file name extension for 'parser' driver.

--- platform/common/config/parser.php
+++ platform/common/config/parser.php
@@ -15,6 +15,8 @@ $config['parser_driver'] = 'parser';
 // Additional valid drivers that may be loaded.
 $config['parser_valid_drivers'] = array(
     'mustache',
+    'lex',
+    'parser',
     'textile',
     'markdown',
     'markdownify',
@@ -27,15 +29,15 @@ $config['parser_valid_drivers'] = array(
     'scss',
     'ts',
     'jsonmin',
-    'lex',
 );
 
 // File extensions associated with parsers that are to be applied
 // to views, partials, layouts.
 // Don't add leading dots on specifying file extensions.
 $config['parser_file_extensions'] = array(
-    'lex' => array('lex.html', 'lex'),
     'mustache' => array('mustache.html', 'mustache'),
+    'lex' => array('lex.html', 'lex'),
+    'parser' => array('parser.php'),
     'markdown' => array('md', 'markdown', 'fbmd'),
     'textile' => 'textile',
 );
