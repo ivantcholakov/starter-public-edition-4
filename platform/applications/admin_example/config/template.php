fd1d7b26249aab3b70a8f3fa7bee0daca5809e26
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-18 13:35:50 +0200

    Enable parser 'i18n' globally.

--- platform/applications/admin_example/config/template.php
+++ platform/applications/admin_example/config/template.php
@@ -1,5 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
 
-$config['theme'] = 'front_theme_bs';
 $config['parser_enabled'] = array('i18n');
 $config['parser_body_enabled'] = array('i18n');
