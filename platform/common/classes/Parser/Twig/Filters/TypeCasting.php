3a03e6c8f0d0dccc8a62fc660381fa196f053c21
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-02-01 05:44:45 +0200

    Twig parser: Adding "Type Casting" section within the demo/testing page, adding the corresponding Twig filters.

---
+++ platform/common/classes/Parser/Twig/Filters/TypeCasting.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * This is an integration class for CodeIgniter.
+ *
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2016
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ */
+
+class Parser_Twig_Filters_TypeCasting {
+
+    public static function boolean($value) {
+
+        return (bool) $value;
+    }
+
+    public static function integer($value) {
+
+        return (int) $value;
+    }
+
+    public static function float($value) {
+
+        return (float) $value;
+    }
+
+    public static function string($value) {
+
+        return (string) $value;
+    }
+
+    public static function twig_array($value) {
+
+        return (array) $value;
+    }
+
+    public static function object($value) {
+
+        return (object) $value;
+    }
+
+    public static function null($value) {
+
+        return null;
+    }
+
+}
--- platform/common/config/parser_twig.php
+++ platform/common/config/parser_twig.php
@@ -34,10 +34,24 @@ $config['extensions'] = array(
 
 // Extending the Twig parser: Extra-functions.
 $config['functions'] = array(
+    // Debugging Previews
     array('print_d', 'print_d', array('is_safe' => array('html')), ENVIRONMENT !== 'production'),
     array('print_r', array('Parser_Twig_Functions_Debug', 'print_r'), array('is_safe' => array('html')), ENVIRONMENT !== 'production'),
     array('var_export', array('Parser_Twig_Functions_Debug', 'var_export'), array('is_safe' => array('html')), ENVIRONMENT !== 'production'),
 );
 
 // Extending the Twig parser: Extra-filters.
-$config['filters'] = array();
+$config['filters'] = array(
+    // Type Casting
+    array('boolean', array('Parser_Twig_Filters_TypeCasting', 'boolean'), array('is_safe' => array('html'))),
+    array('bool', array('Parser_Twig_Filters_TypeCasting', 'boolean'), array('is_safe' => array('html'))),
+    array('integer', array('Parser_Twig_Filters_TypeCasting', 'integer'), array('is_safe' => array('html'))),
+    array('int', array('Parser_Twig_Filters_TypeCasting', 'integer'), array('is_safe' => array('html'))),
+    array('float', array('Parser_Twig_Filters_TypeCasting', 'float'), array('is_safe' => array('html'))),
+    array('double', array('Parser_Twig_Filters_TypeCasting', 'float'), array('is_safe' => array('html'))),
+    array('real', array('Parser_Twig_Filters_TypeCasting', 'float'), array('is_safe' => array('html'))),
+    array('string', array('Parser_Twig_Filters_TypeCasting', 'string')),
+    array('array', array('Parser_Twig_Filters_TypeCasting', 'twig_array')),
+    array('object', array('Parser_Twig_Filters_TypeCasting', 'object')),
+    array('null', array('Parser_Twig_Filters_TypeCasting', 'null'), array('is_safe' => array('html'))),
+);
