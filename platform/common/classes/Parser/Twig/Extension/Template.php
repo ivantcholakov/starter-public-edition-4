54db7d73e818d21e3f3f8beb501cbb2c8fbb1869
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-02-10 22:03:26 +0200

    Twig parser: Template section (1).

---
+++ platform/common/classes/Parser/Twig/Extension/Template.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * This is an integration class for CodeIgniter.
+ *
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2016
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ */
+
+class Parser_Twig_Extension_Template {
+
+    public static function html_title() {
+
+        ob_start();
+
+        template_title();
+
+        return ob_get_clean();
+    }
+
+    public static function metadata() {
+
+        ob_start();
+
+        template_metadata();
+
+        return ob_get_clean();
+    }
+
+}
