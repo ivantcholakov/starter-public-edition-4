9aec78380a81c519d1aa2ceae5b24b3561421e77
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-02-17 03:35:18 +0200

    Admin sample site: Switching to Twig views.

--- platform/applications/admin_example/config/template.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed.');
-
-$config['parser_enabled'] = array('i18n');
-$config['parser_body_enabled'] = array('i18n');
--- platform/applications/admin_example/views/partials/admin_example_footer.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-    <div id="footer">
-        <div class="container">
-            <p class="text-muted">Ivan Tcholakov, 2014. Many thanks to <a href="http://iridadesign.com" target="_blank">Irida Design OOD</a> for sponsoring this project.</p>
-        </div>
-    </div>
