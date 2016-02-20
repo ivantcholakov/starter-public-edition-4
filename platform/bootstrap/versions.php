62e21eece734710fd84953690e7fe6ea9517e40d
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-02-20 17:34:10 +0200

    RELEASE 4.0.119.

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -11,4 +11,4 @@ define('CI_VERSION', '3.0.4');
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.0.119-dev');
+define('PLATFORM_VERSION', '4.0.119');
