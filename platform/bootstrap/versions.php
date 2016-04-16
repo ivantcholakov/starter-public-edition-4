83a3cadc19f99190228a7060c563efe5763ee964
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-04-17 02:01:54 +0300

    Setting the version number for the next release.

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -11,4 +11,4 @@ define('CI_VERSION', '3.0.6');
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.0.135');
+define('PLATFORM_VERSION', '4.0.136-dev');
