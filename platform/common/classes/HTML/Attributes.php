44db9cf56de5162644f5d68538f9258a96bb2b8a
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-04-29 21:08:12 +0300

    Prevent the common functions html_attr_*() from escaping the special HTML characters multiple times.

--- platform/common/classes/HTML/Attributes.php
+++ platform/common/classes/HTML/Attributes.php
@@ -16,6 +16,43 @@ class HTML_Attributes extends HTML_Common2 {
         parent::__construct($attributes);
     }
 
+    /**
+     * Creates HTML attribute string from array
+     *
+     * @param array $attributes Attribute array
+     *
+     * @return string Attribute string
+     */
+    protected static function getAttributesString(array $attributes)
+    {
+        $str     = '';
+        $charset = self::getOption('charset');
+        foreach ($attributes as $key => $value) {
+            // Modified by Ivan Tcholakov, 28-APR-2016.
+            // Prevent multiple escaping.
+            //$str .= ' ' . $key . '="' . htmlspecialchars($value, ENT_QUOTES, $charset) . '"';
+            $str .= ' ' . $key . '="' . htmlspecialchars($value, ENT_QUOTES, $charset, false) . '"';
+            //
+        }
+        return $str;
+    }
+
+    /**
+     * Returns the attribute array or string
+     *
+     * @param bool $asString Whether to return attributes as string
+     *
+     * @return array|string
+     */
+    public function getAttributes($asString = false)
+    {
+        if ($asString) {
+            return self::getAttributesString($this->attributes);
+        } else {
+            return $this->attributes;
+        }
+    }
+
     public function __toString() {
 
         return $this->getAttributes(true);
