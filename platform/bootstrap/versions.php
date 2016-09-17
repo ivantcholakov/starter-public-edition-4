bbce31a776d1b6c8405720dd9d1fbe658d157306
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-09-17 19:01:10 +0300

    RELEASE 4.1.1. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -11,7 +11,7 @@ define('CI_VERSION', '3.1.0');
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.1.0');
+define('PLATFORM_VERSION', '4.1.1');
 
 
 /**
