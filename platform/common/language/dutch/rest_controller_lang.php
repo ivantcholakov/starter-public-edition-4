b26199d42a7db9e6dda0bafcd594f7e9514723d4
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-11-01 23:22:28 +0200

    REST_Controller: Dutch tramslation. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/language/dutch/rest_controller_lang.php
+++ platform/common/language/dutch/rest_controller_lang.php
@@ -1,18 +1,16 @@
 <?php
-
 /*
-* Dutch language
-*/
-
-$lang['text_rest_invalid_api_key'] = 'Invalid API key %s'; // %s is the REST API key
-$lang['text_rest_invalid_credentials'] = 'Invalid credentials';
-$lang['text_rest_ip_denied'] = 'IP denied';
-$lang['text_rest_ip_unauthorized'] = 'IP unauthorized';
-$lang['text_rest_unauthorized'] = 'Unauthorized';
-$lang['text_rest_ajax_only'] = 'Only AJAX requests are allowed';
-$lang['text_rest_api_key_unauthorized'] = 'This API key does not have access to the requested controller';
-$lang['text_rest_api_key_permissions'] = 'This API key does not have enough permissions';
-$lang['text_rest_api_key_time_limit'] = 'This API key has reached the time limit for this method';
-$lang['text_rest_ip_address_time_limit'] = 'This IP Address has reached the time limit for this method';
-$lang['text_rest_unknown_method'] = 'Unknown method';
-$lang['text_rest_unsupported'] = 'Unsupported protocol';
+ * Dutch language
+ */
+$lang['text_rest_invalid_api_key'] 		 = 'Ongeldige API sleutel %s'; // %s is the REST API key
+$lang['text_rest_invalid_credentials']   = 'Ongeldige gegevens';
+$lang['text_rest_ip_denied'] 			 = 'IP-adres geweigerd';
+$lang['text_rest_ip_unauthorized'] 		 = 'IP-adres niet toegestaan';
+$lang['text_rest_unauthorized'] 		 = 'Niet toegestaan';
+$lang['text_rest_ajax_only'] 			 = 'Alleen AJAX requests zijn toegestaan';
+$lang['text_rest_api_key_unauthorized']  = 'De API sleutel heeft geen toegang tot de gevraagde informatie';
+$lang['text_rest_api_key_permissions']   = 'De API sleutel heeft niet genoeg bevoegdheden';
+$lang['text_rest_api_key_time_limit'] 	 = 'De API sleutel heeft de tijdslimiet bereikt';
+$lang['text_rest_ip_address_time_limit'] = 'Het IP-adres heeft de tijdslimiet bereikt';
+$lang['text_rest_unknown_method'] 	     = 'Onbekende actie';
+$lang['text_rest_unsupported'] 			 = 'Protocol wordt niet ondersteund';
