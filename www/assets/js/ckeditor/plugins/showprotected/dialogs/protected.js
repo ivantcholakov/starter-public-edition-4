d382a9267db2a76f1d90f6f5af57b81d0091e0d5
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-12-25 04:27:06 +0200

    CKEditor: A vosual enhancement of 'showprotected' plugin, see https://gist.github.com/BrianGilbert/c41dd35692c3fd7e12b6 Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- www/assets/js/ckeditor/plugins/showprotected/dialogs/protected.js
+++ www/assets/js/ckeditor/plugins/showprotected/dialogs/protected.js
@@ -3,13 +3,13 @@ CKEDITOR.dialog.add( 'showProtectedDialog', function( editor ) {
 
 	return {
 		title: 'Edit Protected Source',
-		minWidth: 300,
-		minHeight: 60,
+		minWidth: 500,
+		minHeight: 400,
 		onOk: function() {
 			var newSourceValue = this.getContentElement( 'info', 'txtProtectedSource' ).getValue();
-			
+
 			var encodedSourceValue = CKEDITOR.plugins.showprotected.encodeProtectedSource( newSourceValue );
-			
+
 			CKEDITOR.plugins.showprotected.selectedElement.$.previousSibling.nodeValue = encodedSourceValue;
 			CKEDITOR.plugins.showprotected.selectedElement.setAttribute('title', newSourceValue);
 			CKEDITOR.plugins.showprotected.selectedElement.setAttribute('alt', newSourceValue);
@@ -32,10 +32,12 @@ CKEDITOR.dialog.add( 'showProtectedDialog', function( editor ) {
 			accessKey: 'I',
 			elements: [
 				{
-				type: 'text',
+				type: 'textarea',
 				id: 'txtProtectedSource',
 				label: 'Value',
 				required: true,
+				rows: 26,
+				style: 'width:100%',
 				validate: function() {
 					if ( !this.getValue() ) {
 						alert( 'The value cannot be empty' );
