2762944928aacd9d0e2ef2e5f736fef0ff481ee1
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-12-25 04:26:48 +0200

    The Playground: Updating the CKEditor demo. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/modules/playground/controllers/online_editor/Admin_mode_controller.php
+++ platform/applications/front/modules/playground/controllers/online_editor/Admin_mode_controller.php
@@ -28,6 +28,12 @@ class Admin_mode_controller extends Playground_Base_Controller {
     public function index() {
 
         $content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+        $content .= '<br />';
+        $content .= '<br />';
+        $content .= '<strong>Twig parsed content:</strong>';
+        $content .= '<br />';
+        $content .= '{{ widget(\'contact_info_widget\') }}';
+        $content .= '<br />';
 
         $validation_rules = array(
             array(
