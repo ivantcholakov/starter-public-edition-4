a4f4087c8880f824e4ab40c0665e593ee3ab65b1
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-12-27 02:47:09 +0200

    Delete temporarily all the Composer packages. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/vendor/my_autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// See https://github.com/composer/composer/issues/4340
-
-require_once __DIR__ . '/composer' . '/my_autoload_real.php';
-
-return ComposerAutoloaderInitdc3d756b09e56e386c98d22248d033e5::getLoader();
