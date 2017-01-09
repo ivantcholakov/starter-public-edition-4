cf6ab0ee22458b80643c50bfbead5a641014347d
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-01-10 01:17:10 +0200

    Custom Email library: Updating version information. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/libraries/Email.php
+++ platform/common/libraries/Email.php
@@ -2,13 +2,13 @@
 
 /**
  * CodeIgniter compatible email-library powered by PHPMailer.
- * Version: 1.2.18
- * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2016.
+ * Version: 1.2.19
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2017.
  * @license The MIT License (MIT), http://opensource.org/licenses/MIT
  * @link https://github.com/ivantcholakov/codeigniter-phpmailer
  *
- * Tested on CodeIgniter 3.1.2+ (December 16th, 2016) and
- * PHPMailer Version 5.2.21 (December 28th, 2016).
+ * Tested on CodeIgniter 3.1.3 (January 9th, 2017) and
+ * PHPMailer Version 5.2.22 (January 5th, 2017).
  */
 
 class Email extends CI_Email {
