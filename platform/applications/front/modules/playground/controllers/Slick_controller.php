5c38e0b9dfb25cc7afd8a5381448ed25117eed8d
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-01-11 20:59:46 +0200

    Extending the Slick slider demo with a second example. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/modules/playground/controllers/Slick_controller.php
+++ platform/applications/front/modules/playground/controllers/Slick_controller.php
@@ -11,11 +11,8 @@ class Slick_controller extends Playground_Base_Controller {
 
         parent::__construct();
 
-        $title = 'Slick Carousel Test';
-
         $this->template
-            ->append_title($title)
-            ->set_breadcrumb($title, site_url('playground/slick'));
+            ->set_partial('subnavbar', 'slick_subnavbar')
         ;
 
         $this->registry->set('nav', 'playground');
@@ -23,10 +20,28 @@ class Slick_controller extends Playground_Base_Controller {
 
     public function index() {
 
+        $title = 'Slick Carousel Test';
+
         $this->template
+            ->append_title($title)
+            ->set_breadcrumb($title, site_url('playground/slick'))
+            ->set('subnavbar_item_active', 'basic_example')
             ->set_partial('css', 'slick_css')
             ->set_partial('scripts', 'slick_scripts')
             ->build('slick');
+    }
+
+    public function example_2() {
+
+        $title = 'Slick Carousel Test 2';
+
+        $this->template
+            ->append_title($title)
+            ->set_breadcrumb($title, site_url('playground/slick'))
+            ->set('subnavbar_item_active', 'example_2')
+            ->set_partial('css', 'slick_2_css')
+            ->set_partial('scripts', 'slick_2_scripts')
+            ->build('slick_2');
     }
 
 }
