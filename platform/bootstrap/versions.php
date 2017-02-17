b3f06c59952e21a333e8942f190dbea1847c10df
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-02-17 21:53:30 +0200

    RELEASE 4.2.39. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -11,7 +11,7 @@ define('CI_VERSION', '3.1.3');
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.2.38');
+define('PLATFORM_VERSION', '4.2.39');
 
 
 /**
