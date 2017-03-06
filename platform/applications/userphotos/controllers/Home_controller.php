d57a004cdc8e84fb41fe5f6b027daa27c22cc836
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-03-06 21:57:06 +0200

    "userphotos" application: Minor code refactoring. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/userphotos/controllers/Home_controller.php
+++ platform/applications/userphotos/controllers/Home_controller.php
@@ -1,32 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
 
-class Home_controller extends Core_Controller {
-
-    protected $base_path;
-
-    protected $default_file;
-    protected $default_base_path;
-
-    protected $blank_file;
-    protected $blank_base_path;
+class Home_controller extends Base_Controller {
 
     public function __construct() {
 
         parent::__construct();
-
-        $this->load->config('custom_user_photo');
-
-        $this->base_path = (string) $this->config->item('custom_user_photo_base_path');
-
-        if ($this->base_path == '') {
-            $this->base_path = PLATFORM_UPLOAD_PATH.'userphotos/';
-        }
-
-        $this->default_file = 'default-person.png';
-        $this->default_base_path = DEFAULTFCPATH.'assets/img/lib/';
-
-        $this->blank_file = 'blank.png';
-        $this->blank_base_path = DEFAULTFCPATH.'assets/img/lib/';
     }
 
     public function index() {
