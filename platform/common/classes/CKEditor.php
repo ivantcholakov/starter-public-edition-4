d9f54710d20a17ce0acb2c368489f1edc71d733f
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-03-14 07:05:35 +0200

    Updating CKEditor, v4.6.2. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/classes/CKEditor.php
+++ platform/common/classes/CKEditor.php
@@ -20,11 +20,11 @@ class CKEditor
     /**
      * The version of %CKEditor.
      */
-    const version = '4.6.1';
+    const version = '4.6.2';
     /**
      * A constant string unique for each release of %CKEditor.
      */
-    const timestamp = '580bcaf';
+    const timestamp = '20af917';
 
     /**
      * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
      * A string indicating the creation date of %CKEditor.
      * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
      */
-    public $timestamp = "580bcaf";
+    public $timestamp = "20af917";
     /**
      * An array that holds event listeners.
      */
