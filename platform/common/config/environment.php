d6e0ce81ca6c09a0a49c9951f6fc57b3e5201fb6
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-10-18 04:40:21 +0300

    Session security related changes. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/config/environment.php
+++ platform/common/config/environment.php
@@ -34,6 +34,19 @@ if (IS_CLI) {
 
     // http://www.controlstyle.com/articles/programming/text/if-mod-since-php/
     session_cache_limiter('private_no_expire');
+
+    // https://stackoverflow.com/questions/22221807/session-cookies-http-secure-flag-how-do-you-set-these
+
+    // **PREVENTING SESSION HIJACKING**
+    // Prevents javascript XSS attacks aimed to steal the session ID
+    ini_set('session.cookie_httponly', 1);
+
+    // **PREVENTING SESSION FIXATION**
+    // Session ID cannot be passed through URLs
+    ini_set('session.use_only_cookies', 1);
+
+    // Uses a secure connection (HTTPS) if possible
+    ini_set('session.cookie_secure', 1);
 }
 
 ini_set('pcre.backtrack_limit', 10000000);
