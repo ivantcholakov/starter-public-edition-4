1e95c1cb950116fd3e72d7b856ad83c304fa0d0b
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-12-12 22:06:25 +0200

    Default error html-templates: Increasing font sizes. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/views/errors/html/error_404.php
+++ platform/common/views/errors/html/error_404.php
@@ -13,8 +13,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 body {
     background-color: #fff;
     margin: 40px;
-    font: 13px/20px normal Helvetica, Arial, sans-serif;
+    font: 16px/20px normal Helvetica, Arial, sans-serif;
     color: #4F5155;
+    word-wrap: break-word;
 }
 
 a {
@@ -27,7 +28,7 @@ h1 {
     color: #444;
     background-color: transparent;
     border-bottom: 1px solid #D0D0D0;
-    font-size: 19px;
+    font-size: 24px;
     font-weight: normal;
     margin: 0 0 14px 0;
     padding: 14px 15px 10px 15px;
@@ -35,7 +36,7 @@ h1 {
 
 code {
     font-family: Consolas, Monaco, Courier New, Courier, monospace;
-    font-size: 12px;
+    font-size: 16px;
     background-color: #f9f9f9;
     border: 1px solid #D0D0D0;
     color: #002166;
