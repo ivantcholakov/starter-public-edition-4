befed2d672e9f4615d30321f2a67455d0c68fdab
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-12-13 05:50:17 +0200

    Default error html-templates: Increasing font sizes. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/views/errors/html/error_exception.php
+++ platform/common/views/errors/html/error_exception.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;font: 16px/20px normal Helvetica, Arial, sans-serif;">
 
 <h4>An uncaught Exception was encountered</h4>
 
