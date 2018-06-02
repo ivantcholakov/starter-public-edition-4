632aeefc965db9344fac1193ca5b454a18fb9cd2
Ivan Tcholakov <ivantcholakov@gmail.com>
2018-06-03 00:56:51 +0300

    CI5516. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/helpers/MY_html_helper.php
+++ platform/common/helpers/MY_html_helper.php
@@ -292,3 +292,131 @@ if (!function_exists('antibot'))
     }
 
 }
+
+
+// Patches from 3.1.9-dev. Remove them after the next CI update. ---------------
+
+if ( ! function_exists('img'))
+{
+    /**
+     * Image
+     *
+     * Generates an <img /> element
+     *
+     * @param    mixed
+     * @param    bool
+     * @param    mixed
+     * @return    string
+     */
+    function img($src = '', $index_page = FALSE, $attributes = '')
+    {
+        if ( ! is_array($src) )
+        {
+            $src = array('src' => $src);
+        }
+
+        // If there is no alt attribute defined, set it to an empty string
+        if ( ! isset($src['alt']))
+        {
+            $src['alt'] = '';
+        }
+
+        $img = '<img';
+
+        foreach ($src as $k => $v)
+        {
+            if ($k === 'src' && ! preg_match('#^(data:[a-z,;])|(([a-z]+:)?(?<!data:)//)#i', $v))
+            {
+                if ($index_page === TRUE)
+                {
+                    $img .= ' src="'.get_instance()->config->site_url($v).'"';
+                }
+                else
+                {
+                    $img .= ' src="'.get_instance()->config->base_url($v).'"';
+                }
+            }
+            else
+            {
+                $img .= ' '.$k.'="'.$v.'"';
+            }
+        }
+
+        return $img._stringify_attributes($attributes).' />';
+    }
+}
+
+if ( ! function_exists('link_tag'))
+{
+    /**
+     * Link
+     *
+     * Generates link to a CSS file
+     *
+     * @param    mixed    stylesheet hrefs or an array
+     * @param    string    rel
+     * @param    string    type
+     * @param    string    title
+     * @param    string    media
+     * @param    bool    should index_page be added to the css path
+     * @return    string
+     */
+    function link_tag($href = '', $rel = 'stylesheet', $type = 'text/css', $title = '', $media = '', $index_page = FALSE)
+    {
+        $CI =& get_instance();
+        $link = '<link ';
+
+        if (is_array($href))
+        {
+            foreach ($href as $k => $v)
+            {
+                if ($k === 'href' && ! preg_match('#^([a-z]+:)?//#i', $v))
+                {
+                    if ($index_page === TRUE)
+                    {
+                        $link .= 'href="'.$CI->config->site_url($v).'" ';
+                    }
+                    else
+                    {
+                        $link .= 'href="'.$CI->config->base_url($v).'" ';
+                    }
+                }
+                else
+                {
+                    $link .= $k.'="'.$v.'" ';
+                }
+            }
+        }
+        else
+        {
+            if (preg_match('#^([a-z]+:)?//#i', $href))
+            {
+                $link .= 'href="'.$href.'" ';
+            }
+            elseif ($index_page === TRUE)
+            {
+                $link .= 'href="'.$CI->config->site_url($href).'" ';
+            }
+            else
+            {
+                $link .= 'href="'.$CI->config->base_url($href).'" ';
+            }
+
+            $link .= 'rel="'.$rel.'" type="'.$type.'" ';
+
+            if ($media !== '')
+            {
+                $link .= 'media="'.$media.'" ';
+            }
+
+            if ($title !== '')
+            {
+                $link .= 'title="'.$title.'" ';
+            }
+        }
+
+        return $link."/>\n";
+    }
+}
+
+// -----------------------------------------------------------------------------
