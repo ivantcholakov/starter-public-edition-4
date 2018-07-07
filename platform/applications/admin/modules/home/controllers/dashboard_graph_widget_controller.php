4d6d1207b4bd4bdd6e3ba0d509f29958269f6e17
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-07 00:31:12 -0500

    Update dashboard_graph_widget_controller.php

--- platform/applications/admin/modules/home/controllers/dashboard_graph_widget_controller.php
+++ platform/applications/admin/modules/home/controllers/dashboard_graph_widget_controller.php
@@ -5,21 +5,25 @@ class Dashboard_graph_widget_controller extends Base_Widget_Controller {
     }
     public function index() {
 
-        // Fake values:
-        $total_page_count = 8;
-        $total_news_count = 1;
-        $total_gallery_count = 5;
-        $total_slideshow_count = 2;
-        $total_category_count = 5;
-        $total_product_count = 50;
-        $data = compact(
-            'total_page_count',
-            'total_news_count',
-            'total_gallery_count',
-            'total_slideshow_count',
-            'total_category_count',
-            'total_product_count'
+        $this->load->library('letter_avatar');
+        $actors = array(
+            array('name' => 'Charles Bronson'),
+            array('name' => 'Humphrey Bogart'),
+            array('name' => 'Marlon Brando'),
+            array('name' => 'Paul Newman'),
+            array('name' => 'Robert Mitchum'),
+            array('name' => 'Roy Scheider'),
+            array('name' => 'Steve McQueen'),
+            array('name' => 'Toshiro Mifune'),
+            array('name' => 'Yul Brynner'),
+            array('name' => 'Николай Волев'),
+            array('name' => 'Теди Москов'),
         );
+        $this->template
+            ->set('actors', $actors)
+            ->set('letter_avatar', $this->letter_avatar)
+            ->build('letter_avatar');
+        
         $this->load->view('dashboard_graph_widget', $data);
     }
 }
