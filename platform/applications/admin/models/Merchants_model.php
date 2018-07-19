46782872be7b84c77f8c352c7e9e76504d96db31
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-19 04:56:26 -0500

    Create Merchants_model.php

---
+++ platform/applications/admin/models/Merchants_model.php
@@ -0,0 +1,366 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Merchants_model extends CI_Model {
+
+    /**
+     * @vars
+     */
+    private $_db;
+
+    /**
+     * Constructor
+     */
+    function __construct()
+    {
+        parent::__construct();
+
+        // define primary table
+        $this->_db = 'merchants';
+    }
+	
+	function get_all($limit = 0, $offset = 0, $filters = array(), $sort = 'dir', $dir = 'ASC')
+    {
+        $sql = "
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM {$this->_db}
+						WHERE id > '0'
+        ";
+
+        if ( ! empty($filters))
+        {
+            foreach ($filters as $key=>$value)
+            {
+                $value = $this->db->escape('%' . $value . '%');
+                $sql .= " AND {$key} LIKE {$value}";
+            }
+        }
+
+        $sql .= " ORDER BY {$sort} {$dir}";
+
+        if ($limit)
+        {
+            $sql .= " LIMIT {$offset}, {$limit}";
+        }
+
+        $query = $this->db->query($sql);
+
+        if ($query->num_rows() > 0)
+        {
+            $results['results'] = $query->result_array();
+        }
+        else
+        {
+            $results['results'] = NULL;
+        }
+
+        $sql = "SELECT FOUND_ROWS() AS total";
+        $query = $this->db->query($sql);
+        $results['total'] = $query->row()->total;
+
+        return $results;
+    }
+	
+	function get_active($limit = 0, $offset = 0, $filters = array(), $sort = 'dir', $dir = 'ASC')
+    {
+        $sql = "
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM {$this->_db}
+						WHERE status = '1'
+        ";
+
+        if ( ! empty($filters))
+        {
+            foreach ($filters as $key=>$value)
+            {
+                $value = $this->db->escape('%' . $value . '%');
+                $sql .= " AND {$key} LIKE {$value}";
+            }
+        }
+
+        $sql .= " ORDER BY {$sort} {$dir}";
+
+        if ($limit)
+        {
+            $sql .= " LIMIT {$offset}, {$limit}";
+        }
+
+        $query = $this->db->query($sql);
+
+        if ($query->num_rows() > 0)
+        {
+            $results['results'] = $query->result_array();
+        }
+        else
+        {
+            $results['results'] = NULL;
+        }
+
+        $sql = "SELECT FOUND_ROWS() AS total";
+        $query = $this->db->query($sql);
+        $results['total'] = $query->row()->total;
+
+        return $results;
+    }
+	
+	function get_moderation($limit = 0, $offset = 0, $filters = array(), $sort = 'dir', $dir = 'ASC')
+    {
+        $sql = "
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM {$this->_db}
+						WHERE status = '2'
+        ";
+
+        if ( ! empty($filters))
+        {
+            foreach ($filters as $key=>$value)
+            {
+                $value = $this->db->escape('%' . $value . '%');
+                $sql .= " AND {$key} LIKE {$value}";
+            }
+        }
+
+        $sql .= " ORDER BY {$sort} {$dir}";
+
+        if ($limit)
+        {
+            $sql .= " LIMIT {$offset}, {$limit}";
+        }
+
+        $query = $this->db->query($sql);
+
+        if ($query->num_rows() > 0)
+        {
+            $results['results'] = $query->result_array();
+        }
+        else
+        {
+            $results['results'] = NULL;
+        }
+
+        $sql = "SELECT FOUND_ROWS() AS total";
+        $query = $this->db->query($sql);
+        $results['total'] = $query->row()->total;
+
+        return $results;
+    }
+	
+	function get_disapproved($limit = 0, $offset = 0, $filters = array(), $sort = 'dir', $dir = 'ASC')
+    {
+        $sql = "
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM {$this->_db}
+						WHERE status = '3'
+        ";
+
+        if ( ! empty($filters))
+        {
+            foreach ($filters as $key=>$value)
+            {
+                $value = $this->db->escape('%' . $value . '%');
+                $sql .= " AND {$key} LIKE {$value}";
+            }
+        }
+
+        $sql .= " ORDER BY {$sort} {$dir}";
+
+        if ($limit)
+        {
+            $sql .= " LIMIT {$offset}, {$limit}";
+        }
+
+        $query = $this->db->query($sql);
+
+        if ($query->num_rows() > 0)
+        {
+            $results['results'] = $query->result_array();
+        }
+        else
+        {
+            $results['results'] = NULL;
+        }
+
+        $sql = "SELECT FOUND_ROWS() AS total";
+        $query = $this->db->query($sql);
+        $results['total'] = $query->row()->total;
+
+        return $results;
+    }
+	
+	function get_list_merchants($limit = 0, $offset = 0, $filters = array(), $sort = 'dir', $dir = 'ASC', $user = NULL)
+    {
+        $sql = "
+            SELECT SQL_CALC_FOUND_ROWS *
+            FROM {$this->_db}
+						WHERE user = '{$user}'
+        ";
+
+        if ( ! empty($filters))
+        {
+            foreach ($filters as $key=>$value)
+            {
+                $value = $this->db->escape('%' . $value . '%');
+                $sql .= " AND {$key} LIKE {$value}";
+            }
+        }
+
+        $sql .= " ORDER BY {$sort} {$dir}";
+
+        if ($limit)
+        {
+            $sql .= " LIMIT {$offset}, {$limit}";
+        }
+
+        $query = $this->db->query($sql);
+
+        if ($query->num_rows() > 0)
+        {
+            $results['results'] = $query->result_array();
+        }
+        else
+        {
+            $results['results'] = NULL;
+        }
+
+        $sql = "SELECT FOUND_ROWS() AS total";
+        $query = $this->db->query($sql);
+        $results['total'] = $query->row()->total;
+
+        return $results;
+    }
+	
+	function get_detail_merchants($id = NULL, $username = NULL)
+    {
+        if ($id)
+        {
+            $sql = "
+                SELECT *
+                FROM {$this->_db}
+								WHERE id = " . $this->db->escape($id) . " AND user = " . $this->db->escape($username) . "
+            ";
+
+            $query = $this->db->query($sql);
+
+            if ($query->num_rows())
+            {
+                return $query->row_array();
+            }
+        }
+
+        return FALSE;
+    }	
+	
+	function get_user_merchant($id = NULL)
+    {
+        if ($id)
+        {
+            $sql = "
+                SELECT *
+                FROM {$this->_db}
+                WHERE id = " . $this->db->escape($id) . "
+            ";
+
+            $query = $this->db->query($sql);
+
+            if ($query->num_rows())
+            {
+                return $query->row_array();
+            }
+        }
+
+        return FALSE;
+    }
+	
+	function get_user_merchant_user($user = NULL)
+    {
+        if ($user)
+        {
+            $sql = "
+                SELECT *
+                FROM {$this->_db}
+                WHERE user = " . $this->db->escape($user) . "
+            ";
+
+            $query = $this->db->query($sql);
+
+            if ($query->num_rows())
+            {
+                return $query->row_array();
+            }
+        }
+
+        return FALSE;
+    }
+	
+	function add_merchant($data) 
+		{
+			$this->db->insert("merchants", $data);
+			return $this->db->insert_id();
+		}
+	
+	function get_merchants($id = NULL)
+    {
+        if ($id)
+        {
+            $sql = "
+                SELECT *
+                FROM {$this->_db}
+                WHERE id = " . $this->db->escape($id) . "
+            ";
+
+            $query = $this->db->query($sql);
+
+            if ($query->num_rows())
+            {
+                return $query->row_array();
+            }
+        }
+
+        return FALSE;
+    }
+	
+	/**
+     * Edit ticket
+     *
+     * @param  array $data
+     * @return boolean
+     */
+    function edit_merchant($data = array())
+    {
+        if ($data)
+        {
+            $sql = "
+                UPDATE {$this->_db}
+                SET
+                    date = " . $this->db->escape($data['date']) . ",
+										link = " . $this->db->escape($data['link']) . ",
+										password = " . $this->db->escape($data['password']) . ",
+										status_link = " . $this->db->escape($data['status_link']) . ",
+										comment = " . $this->db->escape($data['comment']) . ",
+										name = " . $this->db->escape($data['name']) . ",
+										user = " . $this->db->escape($data['user']) . "
+                WHERE id = " . $this->db->escape($data['id']) . "
+            ";
+
+            $this->db->query($sql);
+
+            if ($this->db->affected_rows())
+            {
+                return TRUE;
+            }
+        }
+
+        return FALSE;
+    }
+	
+	
+	/**
+     * Update status
+     *
+     * @param  array $data
+     * @return boolean
+     */
+	function update_merchant($id, $data) {
+		$this->db->where("ID", $id)->update("merchants", $data);
+	}
+	
+}
