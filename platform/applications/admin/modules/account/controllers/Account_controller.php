8065cbbf6455c3b931b1b6f0c95360e19185a10c
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-19 06:27:35 -0500

    Create Account_controller.php

---
+++ platform/applications/admin/modules/account/controllers/Account_controller.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+class Home_controller extends Base_Authenticated_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->_set_nav('home');
+    }
+    public function index() {
+        $title = 'Manage Your Account';
+        $this->registry->set('title', $title);
+        $this->_set_header_icon('user');
+        $this->template
+            ->prepend_title($title)
+            ->set_partial('scripts', 'account_scripts')
+            ->build('account');
+    }
+}
