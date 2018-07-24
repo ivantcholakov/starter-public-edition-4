49d88d4dba11e4386d2f43fdd2e8e3e7a10401d7
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-24 15:27:44 -0500

    Update Account_controller.php

--- platform/applications/admin/modules/account/controllers/Account_controller.php
+++ platform/applications/admin/modules/account/controllers/Account_controller.php
@@ -1,11 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
+
 class Account_controller extends Base_Authenticated_Controller {
     public function __construct() {
         parent::__construct();
         $this->_set_nav('account');
     }
+    
     public function index() {
-        $title = 'Manage Your Account';
+        $title = 'My Account';
         $this->registry->set('title', $title);
         $this->_set_header_icon('user');
         $this->template
