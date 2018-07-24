12c72ae78456cd9a6dcaa94e05bc618febcbe639
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-23 21:30:20 -0500

    Update and rename Emailtemplate_model.php to Emailtemplate.php

--- platform/applications/admin/models/Emailtemplate.php
+++ platform/applications/admin/models/Emailtemplate.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Emailtemplate_model extends CI_Model {
+class Emailtemplate extends CI_Model {
 
     /**
      * @vars
