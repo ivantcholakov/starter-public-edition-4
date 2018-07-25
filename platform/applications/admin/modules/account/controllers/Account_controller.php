37d16978af0f5c28948cf54c4a3ee9a22a8a919d
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-25 05:50:04 -0500

    Update Account_controller.php

--- platform/applications/admin/modules/account/controllers/Account_controller.php
+++ platform/applications/admin/modules/account/controllers/Account_controller.php
@@ -3,12 +3,13 @@ class Account_controller extends Base_Authenticated_Controller {
     public function __construct() {
         parent::__construct();
         $this->_set_nav('account');
+        $this->template->set_layout('account');
     }
     public function index() {
         $title = 'Account';
         $this->registry->set('title', $title);
         $this->_set_header_icon('wrench');
-        $this->template->set_layout('account');
+         $this->template
             ->prepend_title($title)
             ->set_partial('scripts', 'account_scripts')
             ->build('account');
