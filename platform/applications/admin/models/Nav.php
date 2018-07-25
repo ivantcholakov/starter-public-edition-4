01e0c29325badb7447259df3c5185c491e96d736
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-25 07:24:56 -0500

    Update Nav.php

--- platform/applications/admin/models/Nav.php
+++ platform/applications/admin/models/Nav.php
@@ -15,10 +15,10 @@ class Nav extends CI_Model {
 
         //----------------------------------------------------------------------
         
-        $nav['payments'] = array('label' => 'Payments', 'icon' => 'credit card icon', 'location' => 'payments');
-        
         $nav['customers'] = array('label' => 'Customers', 'icon' => 'user outline icon', 'location' => 'customers');
         
+        $nav['charges'] = array('label' => 'Charges', 'icon' => 'credit card icon', 'location' => 'charges');
+                
         $nav['transfers'] = array('label' => 'Transfers', 'icon' => 'rocketchat icon', 'location' => 'transfers');
         
         $nav['plans'] = array('label' => 'Plans', 'icon' => 'envelope square icon', 'location' => 'plans');
@@ -44,17 +44,17 @@ class Nav extends CI_Model {
 
         //----------------------------------------------------------------------
 
-        $nav['account/settings'] = array('label' => $this->lang->line('ui_settings'), 'icon' => 'wrench icon', 'location' => '#');
-        $nav['account/settings/languages'] = array('label' => $this->lang->line('ui_languages'), 'icon' => 'translate icon', 'location' => '#', 'parent_id' => 'account/settings');
-        $nav['account/settings/api'] = array('label' => 'API', 'icon' => 'shield icon', 'location' => 'account/settings/api', 'parent_id' => 'account/settings');
-        $nav['account/settings/gateway'] = array('label' => 'Gateway', 'icon' => 'google icon', 'location' => 'account/settings/gateway', 'parent_id' => 'account/settings');
-        $nav['account/settings/email_settings'] = array('label' => 'Email Settings', 'icon' => 'mail outline icon',  'location' => 'account/settings/emails', 'parent_id' => 'account/settings');
+        $nav['settings'] = array('label' => $this->lang->line('ui_settings'), 'icon' => 'wrench icon', 'location' => '#');
+        $nav['settings/languages'] = array('label' => $this->lang->line('ui_languages'), 'icon' => 'translate icon', 'location' => '#', 'parent_id' => 'settings');
+        $nav['settings/api'] = array('label' => 'API', 'icon' => 'shield icon', 'location' => 'settings/api', 'parent_id' => 'account/settings');
+        $nav['settings/gateway'] = array('label' => 'Gateway', 'icon' => 'google icon', 'location' => 'settings/gateway', 'parent_id' => 'account/settings');
+        $nav['settings/email_settings'] = array('label' => 'Email Settings', 'icon' => 'mail outline icon',  'location' => 'settings/emails', 'parent_id' => 'account/settings');
 
         if ($this->settings->get('mailer_enabled')) {
             $nav['settings/email_test'] = array('label' => 'Email Test', 'icon' => 'mail outline icon', 'location' => '#', 'parent_id' => 'account/settings');
         }
 
-        $nav['account/settings/contact_page'] = array('label' => 'Contact Page', 'icon' => 'phone icon', 'location' => '#', 'parent_id' => 'account/settings');
+        $nav['settings/contact_page'] = array('label' => 'Contact Page', 'icon' => 'phone icon', 'location' => '#', 'parent_id' => 'account/settings');
         
         if (function_exists('phpinfo') && $this->config->item('phpinfo_allow')) {
             $nav['account/settings/phpinfo'] = array('label' => 'phpinfo()', 'icon' => 'info circle icon', 'location' => site_url('phpinfo'), 'parent_id' => 'account/settings');
