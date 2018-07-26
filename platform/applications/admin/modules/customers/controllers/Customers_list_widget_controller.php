41fc3d5516b7aace5544f3e7864c372d3a9222a3
Elektropay <3466095+elektropay@users.noreply.github.com>
2018-07-26 06:53:38 -0500

    Update Customers_list_widget_controller.php

--- platform/applications/admin/modules/customers/controllers/Customers_list_widget_controller.php
+++ platform/applications/admin/modules/customers/controllers/Customers_list_widget_controller.php
@@ -4,21 +4,70 @@ class Customers_list_widget_controller extends Base_Widget_Controller {
         parent::__construct();
     }
     public function index() {
+
+    
 /*
-          $this->load->model('products');
-            $total_customer_count = (int) $this->customers
+        $this->load->model('pages');
+        $total_page_count = (int) $this->pages
+            ->select('COUNT(id)')
+            ->as_value()
+            ->first();
+        if ($this->settings->get('has_news')) {
+            $this->load->model('news');
+            $total_news_count = (int) $this->news
                 ->select('COUNT(id)')
                 ->as_value()
                 ->first();
         } else {
-            $total_customer_count = 0;
+            $total_news_count = 0;
+        }
+        if ($this->settings->get('has_gallery')) {
+            $this->load->model('gallery');
+            $total_gallery_count = (int) $this->gallery
+                ->select('COUNT(id)')
+                ->as_value()
+                ->first();
+        } else {
+            $total_gallery_count = 0;
+        }
+        $this->load->model('slideshow');
+        $total_slideshow_count = (int) $this->slideshow
+            ->select('COUNT(id)')
+            ->as_value()
+            ->first();
+        if ($this->settings->get('has_product_catalog')) {
+            $this->load->model('categories');
+            $total_category_count = (int) $this->categories
+                ->select('COUNT(id)')
+                ->as_value()
+                ->first();
+            $this->load->model('products');
+            $total_product_count = (int) $this->products
+                ->select('COUNT(id)')
+                ->as_value()
+                ->first();
+        } else {
+            $total_category_count = 0;
+            $total_product_count = 0;
         }
 */
         // Fake values:
-        $total_customer_count = 50;
+        $total_page_count = 8;
+        $total_news_count = 1;
+        $total_gallery_count = 5;
+        $total_slideshow_count = 2;
+        $total_category_count = 5;
+        $total_product_count = 50;
         $data = compact(
-            'total_customer_count'
-        );
-        $this->load->view('customers_list_widget', $data);
+            'total_page_count',
+            'total_news_count',
+            'total_gallery_count',
+            'total_slideshow_count',
+            'total_category_count',
+            'total_product_count'
+        );    
+        
+        
+        $this->load->view('customers_list_widget');
     }
 }
