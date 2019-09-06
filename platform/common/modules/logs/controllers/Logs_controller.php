e8097fb74a7c05a12f6ac969b4eeba17d098d4c1
Ivan Tcholakov <ivantcholakov@gmail.com>
2019-09-06 09:54:57 +0300

    CORS. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/modules/logs/controllers/Logs_controller.php
+++ platform/common/modules/logs/controllers/Logs_controller.php
@@ -13,6 +13,11 @@ class Logs_controller extends Core_Controller {
 
     public function index() {
 
+        // https://stackoverflow.com/questions/35588699/response-to-preflight-request-doesnt-pass-access-control-check
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, X-Requested-With');
+
         $this->output->set_output($this->logViewer->showLogs());
     }
 
