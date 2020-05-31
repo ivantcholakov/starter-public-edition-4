0c46ae0bbfad6cfb82c78575cbb495854026936f
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-01 01:15:43 +0300

    RELEASE 4.3.47. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -11,7 +11,7 @@ define('CI_VERSION', '3.1.11');
  * Platform (Application Starter) Version
  * @var    string
  */
-define('PLATFORM_VERSION', '4.3.46');
+define('PLATFORM_VERSION', '4.3.47');
 
 
 /**
