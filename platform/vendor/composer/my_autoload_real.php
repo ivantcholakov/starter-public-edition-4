1b455e8a63085b4397fc4047ef4003251ee8c65a
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-05-31 23:53:38 +0300

    Updating a customized composer file. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/vendor/composer/my_autoload_real.php
+++ platform/vendor/composer/my_autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit7ec88c9c84ecdbd075c7338e7e8fd784
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -23,7 +26,7 @@ class ComposerAutoloaderInit7ec88c9c84ecdbd075c7338e7e8fd784
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit7ec88c9c84ecdbd075c7338e7e8fd784', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
