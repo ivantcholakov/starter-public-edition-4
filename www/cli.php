4ec8a20486fd2bc9186f7a209b4a29f789e661af
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-04 00:52:02 +0300

    A minor change. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- www/cli.php
+++ www/cli.php
@@ -3,7 +3,7 @@
 // See Hack 4. Running CodeIgniter from the Command Line, http://net.tutsplus.com/tutorials/php/6-codeigniter-hacks-for-the-masters/
 // Sample usage: php cli.php "controller/method/parameter_1_key/parameter_1_value/parameter_2_key/parameter_2_value"
 
-if (!((PHP_SAPI == 'cli') or defined('STDIN'))) {
+if (!((PHP_SAPI == 'cli') || defined('STDIN'))) {
     die('Command line only!');
 }
 
