32a461e67ad4d0254e28b905b73498361aa735ed
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-05 07:12:02 +0300

    A comment. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
+// This is a temporary file, for convenience
+// while previewing this starter.
+// Remove it after you determine the locations
+// of the directories platform and public for
+// the final product.
 header('Location: ./public', TRUE, '301');
