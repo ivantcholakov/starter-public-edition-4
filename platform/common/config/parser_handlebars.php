19fa4355dd5e936de331e674266508fba632ebbc
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-28 07:14:18 +0300

    Extended configuration file for Handlebars parser. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/config/parser_handlebars.php
+++ platform/common/config/parser_handlebars.php
@@ -1,8 +1,34 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2016
- * @license The MIT License, http://opensource.org/licenses/MIT
- */
+// Filesystem path to the disk cache location.
+$config['cache'] = ENVIRONMENT === 'production' ? HANDLEBARS_CACHE : null;
 
-$config['escape'] = null;
+// Cache file prefix, defaults to empty string.
+$config['cache_file_prefix'] = '';
+
+// Cache file extension, defaults to empty string.
+$config['cache_file_suffix'] = '';
+
+// A Handlebars template loader instance. Uses a StringLoader if not specified.
+$config['loader'] = null;
+
+// A Handlebars loader instance for partials. Uses a StringLoader if not specified.
+$config['partials_loader'] = null;
+
+// An array of alliases of partial names: [['initial_name' => 'alias'], ...]
+// The loader for partials would try to locate them by aliases only, if defined.
+$config['partials_alias'] = array();
+
+// An array of helper functions. Normally a function like
+// function ($sender, $name, $arguments), $arguments is unscaped arguments and
+// is a string, not an array.
+$config['helpers'] = null;
+
+// A callable escape function to use.
+$config['escape'] = 'htmlspecialchars';
+
+// Parametes to pass to the escape function.
+$config['escapeArgs'] = array(ENT_QUOTES, 'UTF-8');
+
+// Enables @data variables (boolean, default: false).
+$config['enableDataVariables'] = false;
