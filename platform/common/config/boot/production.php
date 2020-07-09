ba13916f369c2295fef8338f697423e6982ddce2
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-09 20:29:19 +0300

    Error reporting on production: Let errors under CLI be visible. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/config/boot/production.php
+++ platform/common/config/boot/production.php
@@ -1,11 +1,28 @@
 <?php
 
-/*
-  |--------------------------------------------------------------------------
-  | ERROR DISPLAY
-  |--------------------------------------------------------------------------
-  | Don't show ANY in production environments. Instead, let the system catch
-  | it and display a generic error message.
- */
-ini_set('display_errors', '0');
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+if (!IS_CLI) {
+
+    /*
+      |--------------------------------------------------------------------------
+      | ERROR DISPLAY
+      |--------------------------------------------------------------------------
+      | Don't show ANY in production environments. Instead, let the system catch
+      | it and display a generic error message.
+     */
+    ini_set('display_errors', '0');
+    error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+
+} else {
+
+    /*
+      |--------------------------------------------------------------------------
+      | ERROR DISPLAY
+      |--------------------------------------------------------------------------
+      | In development, we want to show as many errors as possible to help
+      | make sure they don't make it to production. And save us hours of
+      | painful debugging.
+     */
+    error_reporting(-1);
+    ini_set('display_errors', '1');
+
+}
