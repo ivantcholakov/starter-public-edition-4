becf29f9af885fd1fb497d3dcc6d94e31187d151
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-10 08:20:27 +0300

    A configuration file update. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/config/user_agents.php
+++ platform/common/config/user_agents.php
@@ -219,5 +219,6 @@ $robots = array(
     'curious george'     => 'Curious George',
     'ia_archiver'        => 'Alexa Crawler',
     'MJ12bot'            => 'Majestic-12',
-    'Uptimebot'          => 'Uptimebot'
+    'Uptimebot'          => 'Uptimebot',
+    'UptimeRobot'        => 'UptimeRobot'
 );
