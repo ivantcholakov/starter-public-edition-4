5e5b344ad4b1b570df173d051b127e1b323e40e9
Ivan Tcholakov <ivantcholakov@gmail.com>
2021-01-18 23:31:43 +0200

    Web assets: Simple cache busting. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/helpers/template_helper.php
+++ platform/common/helpers/template_helper.php
@@ -578,7 +578,7 @@ if (!function_exists('js_modernizr')) {
     // Modified by Ivan Tcholakov, 08-DEC-2020.
     function js_modernizr() {
 
-        return '    <script type="text/javascript" src="'.html_attr_escape(base_url('assets/composer-asset/components/modernizr/modernizr.js')).'"></script>';
+        return '    <script type="text/javascript" src="'.html_attr_escape(base_url('assets/composer-asset/components/modernizr/modernizr.js?v='.PLATFORM_VERSION)).'"></script>';
     }
 
 }
@@ -589,7 +589,7 @@ if (!function_exists('js_jquery')) {
     // Modified by Ivan Tcholakov, 08-DEC-2020.
     function js_jquery() {
 
-        return '    <script type="text/javascript" src="'.html_attr_escape(base_url('assets/composer-asset/components/jquery/jquery.min.js')).'"></script>';
+        return '    <script type="text/javascript" src="'.html_attr_escape(base_url('assets/composer-asset/components/jquery/jquery.min.js?v='.PLATFORM_VERSION)).'"></script>';
     }
 
 }
