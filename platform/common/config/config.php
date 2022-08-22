0b6ab4aad149c3f50b2ca2d4579991f5017f5a5d
Ivan Tcholakov <ivantcholakov@gmail.com>
2022-08-22 08:19:21 +0300

    Allow unicode in links by default.

--- platform/common/config/config.php
+++ platform/common/config/config.php
@@ -188,7 +188,7 @@ $config['composer_autoload'] = COMPOSER_PATH;
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-=+';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-=+\p{L}';
 
 /*
 |--------------------------------------------------------------------------
